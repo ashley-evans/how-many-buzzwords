02c7ae3b68055588986752bef68a10779b6862d8
Ashley Evans <ashleyev@hotmail.co.uk>
2021-09-02 09:13:33 +0100

    Update deploy.sh to enable multi environment deployment
    Update teardown.sh to stack name to specified during teardown
    Rename integrate.yml to ci.yml
    Add deploy step to ci.yml that deploys to production stack

--- aws/scripts/deploy.sh
+++ aws/scripts/deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+usage() { echo "Usage: -f [Force deployment without changeset check (Optional)] -e [Name of SAM config environment]" 1>&2; exit 1; }
+
+while getopts "fe:" opt; do
+    case $opt in
+        f)
+            force=true
+            ;;
+        e)
+            environment=$OPTARG
+            ;;
+        \?)
+            usage
+            ;;
+    esac
+done
+
+if [ -z $environment ]; then
+    environment="default"
+fi
+
 echo "Deploying S3 Bucket for Buzzword Stack Dependencies"
 
 aws cloudformation deploy --template-file ../templates/buzzword-bucket-template.yml --stack-name buzzword-bucket-stack
@@ -10,4 +30,8 @@ sam build --template-file ../templates/buzzword-template.yml
 
 echo "Deploying Buzzword Stack"
 
-sam deploy --config-file ./samconfig.toml
+if [ $force ]; then
+    sam deploy --config-file ./samconfig.toml --no-confirm-changeset --config-env $environment
+else
+    sam deploy --config-file ./samconfig.toml --config-env $environment
+fi
---
+++ aws/scripts/test-invoke.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl -d "{ \"MessageBody\": \"test1\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test2\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test3\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test4\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test5\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test6\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test7\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test8\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test9\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test10\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
+curl -d "{ \"MessageBody\": \"test11\" }" -H "Content-Type: application/json" -X POST https://09u4joj2u9.execute-api.eu-west-2.amazonaws.com/prod/wibble
