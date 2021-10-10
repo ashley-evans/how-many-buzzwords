27f7e774ae4e329760b53d70fb121033c6de2f33
Ashley Evans <ashleyev@hotmail.co.uk>
2021-10-10 09:05:23 +0100

    Update connection-manager to store connection details on connect request

--- aws/applications/socket/functions/connection-manager/__tests__/connection-manager.test.js
+++ aws/applications/socket/functions/connection-manager/__tests__/connection-manager.test.js
@@ -1,9 +1,17 @@
 const { handler } = require('../connection-manager');
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { mockClient } = require('aws-sdk-client-mock');
+
+const { CONNECT_ROUTE_KEY, DISCONNECT_ROUTE_KEY } = require('../constants');
 
 const EXPECTED_CONNECTION_ID = 'Gyvd8cAwLPECHlQ=';
 const EXPECTED_DOMAIN_NAME = 'test.test.com';
 const EXPECTED_STAGE = 'test';
-const CONNECTED_ROUTE_KEY = '$connect';
+
+const TABLE_NAME = 'test';
+process.env.TABLE_NAME = TABLE_NAME;
+
+const ddbMock = mockClient(DynamoDBClient);
 
 const createEvent = (connectionId, domainName, stage, routeKey) => {
     return {
@@ -16,6 +24,10 @@ const createEvent = (connectionId, domainName, stage, routeKey) => {
     };
 };
 
+beforeEach(() => {
+    ddbMock.reset();
+});
+
 describe('input validation', () => {
     test.each([
         ['event with missing requestContext', {}],
@@ -26,7 +38,7 @@ describe('input validation', () => {
                 undefined,
                 EXPECTED_DOMAIN_NAME,
                 EXPECTED_STAGE,
-                CONNECTED_ROUTE_KEY
+                CONNECT_ROUTE_KEY
             )
         ],
         [
@@ -35,7 +47,7 @@ describe('input validation', () => {
                 EXPECTED_CONNECTION_ID,
                 undefined,
                 EXPECTED_STAGE,
-                CONNECTED_ROUTE_KEY
+                CONNECT_ROUTE_KEY
             )
         ],
         [
@@ -44,7 +56,7 @@ describe('input validation', () => {
                 EXPECTED_CONNECTION_ID,
                 'not a domain',
                 EXPECTED_STAGE,
-                CONNECTED_ROUTE_KEY
+                CONNECT_ROUTE_KEY
             )
         ],
         [
@@ -53,7 +65,7 @@ describe('input validation', () => {
                 EXPECTED_CONNECTION_ID,
                 EXPECTED_DOMAIN_NAME,
                 undefined,
-                CONNECTED_ROUTE_KEY
+                CONNECT_ROUTE_KEY
             )
         ],
         [
@@ -70,6 +82,51 @@ describe('input validation', () => {
             await expect(handler(input)).rejects.toThrowError(
                 'Event object failed validation'
             );
+        }
+    );
+});
+
+describe('incoming connect event', () => {
+    test('stores connection id and endpoint in DynamoDB table', async () => {
+        const event = createEvent(
+            EXPECTED_CONNECTION_ID,
+            EXPECTED_DOMAIN_NAME,
+            EXPECTED_STAGE,
+            CONNECT_ROUTE_KEY
+        );
+        const expectedEndpoint =
+            `https://${EXPECTED_DOMAIN_NAME}/${EXPECTED_STAGE}`;
+
+        await handler(event);
+        const dynamoDbCallsInputs = ddbMock.calls()
+            .map(call => call.args[0].input);
+
+        expect(dynamoDbCallsInputs).toHaveLength(1);
+        expect(dynamoDbCallsInputs).toContainEqual({
+            TableName: TABLE_NAME,
+            Item: {
+                ConnectionId: EXPECTED_CONNECTION_ID,
+                ConnectionEndpoint: expectedEndpoint
+            }
+        });
+    });
+});
+
+describe('incoming disconnect event', () => {
+    test(
+        'does not store connection id and endpoint in DynamoDB table',
+        async () => {
+            const event = createEvent(
+                EXPECTED_CONNECTION_ID,
+                EXPECTED_DOMAIN_NAME,
+                EXPECTED_STAGE,
+                DISCONNECT_ROUTE_KEY
+            );
+
+            await handler(event);
+            const dynamoDbCalls = ddbMock.calls();
+
+            expect(dynamoDbCalls).toHaveLength(0);
         }
     );
 });
--- aws/applications/socket/functions/connection-manager/connection-manager.js
+++ aws/applications/socket/functions/connection-manager/connection-manager.js
@@ -1,5 +1,9 @@
 const middy = require('@middy/core');
 const validator = require('@middy/validator');
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+const ddbClient = new DynamoDBClient({});
+
+const { CONNECT_ROUTE_KEY } = require('./constants');
 
 const INPUT_SCHEMA = {
     type: 'object',
@@ -28,8 +32,28 @@ const INPUT_SCHEMA = {
     }
 };
 
-const baseHandler = (event) => {
-    console.log(JSON.stringify(event));
+const storeIncomingRequest = async (connectionId, domainName, stage) => {
+    const connectionEndpoint = `https://${domainName}/${stage}`;
+    const params = {
+        TableName: process.env.TABLE_NAME,
+        Item: {
+            ConnectionId: connectionId,
+            ConnectionEndpoint: connectionEndpoint
+        }
+    };
+
+    await ddbClient.send(new PutItemCommand(params));
+};
+
+const baseHandler = async (event) => {
+    const requestContext = event.requestContext;
+    if (requestContext.routeKey === CONNECT_ROUTE_KEY) {
+        storeIncomingRequest(
+            requestContext.connectionId,
+            requestContext.domainName,
+            requestContext.stage
+        );
+    }
 };
 
 const handler = middy(baseHandler)
