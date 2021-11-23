6ee5836450b3f80189af1eb6283813134e509a2f
Ashley Evans <ashleyev@hotmail.co.uk>
2021-11-23 19:07:12 +0000

    Update http-mock to accept either string or regexp

--- helpers/http-mock.js
+++ helpers/http-mock.js
@@ -1,9 +1,13 @@
 const nock = require('nock');
 const fs = require('fs-extra');
-const escapeRegExp = require('lodash.escaperegexp');
 
-const mockURLFromFile = (urlText, pathname, filePath, persist) => {
-    const mock = nock(new RegExp(escapeRegExp(urlText)))
+const mockURLFromFile = (urlMatcher, pathname, filePath, persist) => {
+    if (!(urlMatcher instanceof RegExp || typeof urlMatcher === 'string')) {
+        throw new Error(`Provided urlMatcher value: ${urlMatcher} is not a ` +
+            'Regular Expression or a String');
+    }
+
+    const mock = nock(urlMatcher)
         .get(pathname)
         .reply(
             200,
