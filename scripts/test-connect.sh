5f4ca7d336093bd242e6fa48d390a491f223be67
Ashley Evans <ashleyev@hotmail.co.uk>
2021-11-24 16:13:28 +0000

    Update test scripts to refer to prod stack resources

--- scripts/test-connect.sh
+++ scripts/test-connect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wscat -c wss://lo29ztps30.execute-api.eu-west-2.amazonaws.com/prod?BaseUrl=https://www.o2.co.uk/
+wscat -c wss://k39yjiz681.execute-api.eu-west-2.amazonaws.com/prod?BaseUrl=www.o2.co.uk
