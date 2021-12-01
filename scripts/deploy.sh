8cc150dea43407f29c04ece3979b66c96b0a15ff
Ashley Evans <ashleyev@hotmail.co.uk>
2021-12-01 15:44:03 +0000

    Update deploy.sh to enable script to be ran from any location

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,18 +20,21 @@ if [ -z $environment ]; then
     environment="default"
 fi
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+script_parent_dir="$( dirname "$script_dir")"
+
 echo "Deploying S3 Bucket for Buzzword Stack Dependencies"
 
-aws cloudformation deploy --template-file ../templates/buzzword-bucket-template.yml --stack-name buzzword-bucket-stack
+aws cloudformation deploy --template-file "$script_parent_dir"/templates/buzzword-bucket-template.yml --stack-name buzzword-bucket-stack
 
 echo "Building Buzzword Stack"
 
-sam build --template-file ../templates/buzzword-template.yml
+sam build --template-file "$script_parent_dir"/templates/buzzword-template.yml
 
 echo "Deploying Buzzword Stack"
 
 if [ $force ]; then
-    sam deploy --config-file ./samconfig.toml --no-confirm-changeset --config-env $environment
+    sam deploy --config-file "$script_dir"/samconfig.toml --no-confirm-changeset --config-env $environment
 else
-    sam deploy --config-file ./samconfig.toml --config-env $environment
+    sam deploy --config-file "$script_dir"/samconfig.toml --config-env $environment
 fi
