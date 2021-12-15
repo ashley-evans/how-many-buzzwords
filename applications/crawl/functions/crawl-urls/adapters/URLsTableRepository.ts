81d46e0898ae9750f9613d28589ef6af63e59703
Ashley Evans <ashleyev@hotmail.co.uk>
2021-12-15 09:18:32 +0000

    Add URLsTableRepository to store base url and pathnames to DynamoDB

---
+++ applications/crawl/functions/crawl-urls/adapters/URLsTableRepository.ts
@@ -0,0 +1,38 @@
+import Repository from "../ports/Repository";
+import {
+    PutItemCommand,
+    DynamoDBClient,
+    PutItemCommandInput
+} from "@aws-sdk/client-dynamodb";
+import { URLsTableKeyFields } from "buzzword-aws-crawl-common";
+
+class URLsTableRepository implements Repository {
+    private ddbClient;
+    constructor(private tableName: string) {
+        this.ddbClient = new DynamoDBClient({});
+    }
+
+    storePathname(baseURL: string, pathname: string): Promise<boolean> {
+        return new Promise((resolve, reject) => { 
+            const input: PutItemCommandInput = {
+                TableName: this.tableName,
+                Item: {
+                    [URLsTableKeyFields.HashKey]: { S: baseURL },
+                    [URLsTableKeyFields.SortKey]: { S: pathname} 
+                }
+            };
+            const command = new PutItemCommand(input);
+
+            this.ddbClient.send(command)
+                .then(() => {
+                    resolve(true);
+                })
+                .catch((ex: unknown) => {
+                    reject(ex);
+                });
+        });
+
+    }
+}
+
+export default URLsTableRepository;
