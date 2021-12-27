d7dde2448c845e8d59f8685bbcce09dae01575db
Ashley Evans <ashleyev@hotmail.co.uk>
2021-12-27 16:03:20 +0000

    Wrap Keyphrase DB repository storage tests in wrapper to group related tests

--- applications/keyphrase/functions/find-keyphrases/adapters/__tests__/KeyphraseDynamoDBRepository.test.ts
+++ applications/keyphrase/functions/find-keyphrases/adapters/__tests__/KeyphraseDynamoDBRepository.test.ts
@@ -34,96 +34,102 @@ beforeAll(() => {
     jest.spyOn(console, 'log').mockImplementation(() => undefined);
 });
 
-describe.each([
-    [
-        'a single keyphrase occurrence',
+describe('store occurrences', () => {
+
+    describe.each([
         [
-            createKeyphraseOccurrence('keyphrase 1', 1)
-        ]
-    ],
-    [
-        'multiple keyphrases occurrences',
+            'a single keyphrase occurrence',
+            [
+                createKeyphraseOccurrence('keyphrase 1', 1)
+            ]
+        ],
         [
-            createKeyphraseOccurrence('keyphrase 1', 1),
-            createKeyphraseOccurrence('keyphrase 2', 2)
+            'multiple keyphrases occurrences',
+            [
+                createKeyphraseOccurrence('keyphrase 1', 1),
+                createKeyphraseOccurrence('keyphrase 2', 2)
+            ]
         ]
-    ]
-])('given %s', (message: string, occurrences : KeyphraseOccurrences[]) => {
-    let response: boolean;
+    ])('given %s', (message: string, occurrences : KeyphraseOccurrences[]) => {
+        let response: boolean;
 
-    beforeAll(async () => {
-        ddbMock.reset();
-        ddbMock.on(PutItemCommand).resolves({});
+        beforeAll(async () => {
+            ddbMock.reset();
+            ddbMock.on(PutItemCommand).resolves({});
 
-        response = await repository.storeOccurrences(
-            VALID_URL,
-            occurrences
-        );
-    });
+            response = await repository.storeOccurrences(
+                VALID_URL,
+                occurrences
+            );
+        });
 
-    test('stores URL and keyphrases in DynamoDB', async () => {
-        const putItemCommands = ddbMock.commandCalls(PutItemCommand);
+        test('stores URL and keyphrases in DynamoDB', async () => {
+            const putItemCommands = ddbMock.commandCalls(PutItemCommand);
 
-        expect(putItemCommands).toHaveLength(occurrences.length);
-        const commandInputs = putItemCommands.map(
-            command => command.args[0].input
-        );
+            expect(putItemCommands).toHaveLength(occurrences.length);
+            const commandInputs = putItemCommands.map(
+                command => command.args[0].input
+            );
 
-        for (const occurrence of occurrences) {
-            const expectedInput: PutItemCommandInput = {
-                TableName: TABLE_NAME,
-                Item: {
-                    [KeyphraseTableKeyFields.HashKey]: {
-                        S: VALID_URL 
-                    },
-                    [KeyphraseTableKeyFields.SortKey]: {
-                        S: occurrence.keyphrase 
-                    },
-                    [KeyphraseTableNonKeyFields.Occurrence]: {
-                        N: occurrence.occurrences.toString()
+            for (const occurrence of occurrences) {
+                const expectedInput: PutItemCommandInput = {
+                    TableName: TABLE_NAME,
+                    Item: {
+                        [KeyphraseTableKeyFields.HashKey]: {
+                            S: VALID_URL 
+                        },
+                        [KeyphraseTableKeyFields.SortKey]: {
+                            S: occurrence.keyphrase 
+                        },
+                        [KeyphraseTableNonKeyFields.Occurrence]: {
+                            N: occurrence.occurrences.toString()
+                        }
                     }
-                }
-            };
+                };
 
-            expect(commandInputs).toContainEqual(expectedInput);
+                expect(commandInputs).toContainEqual(expectedInput);
+            }
+        });
+
+        test('returns success if dynamoDB call succeeds', () => {
+            expect(response).toBe(true);
+        });
+    });
+
+    test(
+        'throws exception if an error occurs during DynamoDB insert',
+        async () => {
+            const expectedError = new Error('test error');
+            ddbMock.on(PutItemCommand).rejects(expectedError);
+
+            expect.assertions(1);
+            await expect(repository.storeOccurrences(
+                VALID_URL,
+                [createKeyphraseOccurrence('phrase 1', 1)]
+            )).rejects.toEqual(expectedError);
         }
-    });
+    );
 
-    test('returns success if dynamoDB call succeeds', () => {
-        expect(response).toBe(true);
-    });
-});
+    describe('given an empty array of occurrences', () => {
+        let response: boolean;
 
-test('throws exception if an error occurs during DynamoDB insert', async () => {
-    const expectedError = new Error('test error');
-    ddbMock.on(PutItemCommand).rejects(expectedError);
+        beforeAll(async () => {
+            ddbMock.reset();
+            ddbMock.on(PutItemCommand).resolves({});
 
-    expect.assertions(1);
-    await expect(repository.storeOccurrences(
-        VALID_URL,
-        [createKeyphraseOccurrence('phrase 1', 1)]
-    )).rejects.toEqual(expectedError);
-});
-
-describe('given an empty array of occurrences', () => {
-    let response: boolean;
-
-    beforeAll(async () => {
-        ddbMock.reset();
-        ddbMock.on(PutItemCommand).resolves({});
-
-        response = await repository.storeOccurrences(
-            VALID_URL,
-            []
-        );
-    });
+            response = await repository.storeOccurrences(
+                VALID_URL,
+                []
+            );
+        });
 
 
-    test('does not call DynamoDB', () => {
-        expect(ddbMock.calls()).toHaveLength(0);
-    });
+        test('does not call DynamoDB', () => {
+            expect(ddbMock.calls()).toHaveLength(0);
+        });
 
-    test('returns success', () => {
-        expect(response).toBe(true);
+        test('returns success', () => {
+            expect(response).toBe(true);
+        });
     });
 });
