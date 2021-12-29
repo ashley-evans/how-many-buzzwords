7a11e8957d82cf57609523d0f1fc0aa7b9fe8bec
Ashley Evans <ashleyev@hotmail.co.uk>
2021-12-29 13:57:33 +0000

    Fix Keyphrases.ts keyphrase repository import

--- applications/keyphrase/functions/find-keyphrases/domain/Keyphrases.ts
+++ applications/keyphrase/functions/find-keyphrases/domain/Keyphrases.ts
@@ -2,7 +2,7 @@ import HTMLParsingProvider from "../ports/HTMLParsingProvider";
 import HTTPRequestProvider from "../ports/HTTPRequestProvider";
 import KeyphrasesPort from "../ports/KeyphrasePort";
 import { KeyphraseProvider } from "../ports/KeyphraseProvider";
-import KeyphraseRepository from "../ports/KeyphraseRepository";
+import { KeyphraseRepository } from "../ports/KeyphraseRepository";
 
 class Keyphrases implements KeyphrasesPort {
     constructor(
