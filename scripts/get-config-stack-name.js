87c6a58abcb29ec1f2f92ed259fbbde601b2feb6
Ashley Evans <ashleyev@hotmail.co.uk>
2022-01-01 17:49:56 +0000

    Add get-config-stack-name.js script to read SAM toml config and obtain a given environment's stack name
    Update tsconfig to not transpile scripts

---
+++ scripts/get-config-stack-name.js
@@ -0,0 +1,42 @@
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
+const { createReadStream } = require('fs-extra');
+const concat = require('concat-stream');
+const toml = require('toml');
+
+const argv = yargs(hideBin(process.argv))
+    .option('c', {
+        type: 'string',
+        describe: 'Path to configuration file',
+        demandOption: true
+    })
+    .option('e', {
+        type: 'string',
+        describe: 'Name of SAM config environment',
+        default: 'default'
+    })
+    .argv;
+
+
+createReadStream(argv.c, 'utf8').pipe(concat(function(data) {
+    let parsed;
+    try {
+        parsed = toml.parse(data);
+    } catch (ex) {
+        console.error(new Error(`Error occured during parsing ${ex}`));
+        process.exitCode = 1;
+        process.exit();
+    }
+
+    const environmentConfig = parsed[argv.e]?.deploy?.parameters;
+    if (!environmentConfig) {
+        console.error(
+            new Error(`Configuration does not exist for environment ${argv.e}`)
+        );
+
+        process.exitCode = 1;
+        process.exit();
+    }
+
+    console.log(environmentConfig['stack_name']);
+}));
