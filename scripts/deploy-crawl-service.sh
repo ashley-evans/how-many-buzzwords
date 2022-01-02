2b971b5f0e1fc14392b37de57aebe5c9b700e3f2
Ashley Evans <ashleyev@hotmail.co.uk>
2022-01-02 13:43:22 +0000

    Add deploy-crawl-service.sh to build and deploy the crawl service's resources

---
+++ scripts/deploy-crawl-service.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage:
+    -e [Environment to deploy]" 1>&2;
+    exit 1;
+}
+
+while getopts "e:h" opt; do
+    case $opt in
+        e)
+            environment=$OPTARG
+            ;;
+        h)
+            usage
+            ;;
+        ?)
+            usage
+            ;;
+    esac
+done
+
+if [ -z $environment ]; then
+    environment="default"
+fi
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+root_dir="$( dirname "$script_dir")"
+
+template_path="$root_dir/dist/services/crawl/crawl-template.yml"
+if [ ! -f $template_path ]; then
+    echo "Error: Cannot find template in built files."
+    exit 1
+fi
+
+config_path="$root_dir/services/crawl/samconfig.toml"
+if [ ! -f $config_path ]; then
+    echo "Error: Cannot find crawl config file."
+    exit 1
+fi
+
+$script_dir/helpers/deploy-service.sh \
+    -t $template_path \
+    -c $config_path \
+    -e $environment \
+    -f \
+    --cache
