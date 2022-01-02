04210e5dd799ac72e3ced880cf50b5b226cd6284
Ashley Evans <ashleyev@hotmail.co.uk>
2022-01-02 18:12:34 +0000

    Add script to remove all old lambda layer versions prior to retention policy change

---
+++ scripts/helpers/delete-all-lambda-layer-versions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage:
+    -l [Name of layer to delete]" 1>&2;
+    exit 1; 
+}
+
+while getopts "l:h" opt; do
+    case $opt in
+        l)
+            layer=$OPTARG
+            ;;
+        h)
+            usage
+            ;;
+        ?)
+            usage
+            ;;
+    esac
+done
+
+if [ -z $layer ]; then
+    usage 
+fi
+
+versions=$(aws lambda list-layer-versions --layer-name $layer | jq -r '.LayerVersions[] | { Version} | join(" ")')
+
+for version in $versions; do
+    echo "Deleting layer: $layer, Version: $version"
+    aws lambda delete-layer-version --layer-name $layer --version-number $version
+done
