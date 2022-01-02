de23fcd48678941c2941ebf22c3b89c8477d8852
Ashley Evans <ashleyev@hotmail.co.uk>
2022-01-02 14:05:57 +0000

    Update compile.sh to copy any openapi defintions to build folder
    Rename crawl-openapi-template to crawl-openapi to prevent running cfn-lint on openapi definitions

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -36,9 +36,11 @@ $root_dir/node_modules/.bin/tsc --project $root_dir/tsconfig.build.json
 echo "Copying required deployment files to built directory..."
 $root_dir/node_modules/.bin/copyfiles -E \
     applications/**/*-template.yml \
+    applications/**/*-openapi.yml \
     applications/**/package*.json \
     applications/**/Makefile \
     services/**/*-template.yml \
+    services/**/*-openapi.yml \
     services/**/package*.json \
     services/**/Makefile \
     dist
