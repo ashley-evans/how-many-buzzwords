92b89ed7414c88663fe89eec96ca2b63933a9cec
Ashley Evans <ashleyev@hotmail.co.uk>
2022-02-13 11:17:44 +0000

    Add GetURLsDomain to return all pathnames for a given base URL

---
+++ services/crawl/functions/urls-crud/functions/get-urls/domain/GetURLsDomain.ts
@@ -0,0 +1,20 @@
+import { Repository } from 'buzzword-aws-crawl-urls-repository-library';
+
+import { GetURLsPort, PathnameResponse } from '../ports/GetURLsPort';
+
+class GetURLsDomain implements GetURLsPort {
+    constructor(private repository: Repository) {}
+    
+    async getPathnames(baseURL: URL): Promise<PathnameResponse[]> {
+        const pathnames = await this.repository.getPathnames(baseURL.hostname);
+
+        return pathnames.map((pathname) => {
+            return {
+                pathname: pathname.pathname,
+                crawledAt: pathname.createdAt
+            };
+        });
+    }
+}
+
+export default GetURLsDomain;
---
+++ services/crawl/functions/urls-crud/functions/get-urls/domain/__tests__/GetURLsDomain.test.ts
@@ -0,0 +1,65 @@
+import { mock } from 'jest-mock-extended';
+import {
+    Pathname,
+    Repository
+} from 'buzzword-aws-crawl-urls-repository-library';
+
+import GetURLsDomain from '../GetURLsDomain';
+import { PathnameResponse } from '../../ports/GetURLsPort';
+
+const VALID_URL = new URL('https://www.example.com/');
+
+const mockRepository = mock<Repository>();
+const domain = new GetURLsDomain(mockRepository);
+
+function createPathnameEntry(pathname: string): Pathname {
+    const date = new Date();
+    return {
+        pathname,
+        createdAt: date,
+        updatedAt: date
+    };
+}
+
+describe('given a URL that has been crawled before', () => {
+    const pathnames = [
+        createPathnameEntry(VALID_URL.pathname),
+        createPathnameEntry('/test')
+    ];
+
+    let response: PathnameResponse[];
+    
+    beforeAll(async () => {
+        jest.resetAllMocks();
+        mockRepository.getPathnames.mockResolvedValue(pathnames);
+
+        response = await domain.getPathnames(VALID_URL);
+    });
+
+    test('calls repository with hostname from provided URL', () => {
+        expect(mockRepository.getPathnames).toHaveBeenCalledTimes(1);
+        expect(mockRepository.getPathnames).toHaveBeenCalledWith(
+            VALID_URL.hostname
+        );
+    });
+
+    test('returns all pathnames crawled', () => {
+        for (const pathname of pathnames) {
+            expect(response).toContainEqual(
+                expect.objectContaining({
+                    pathname: pathname.pathname
+                })
+            );
+        }
+    });
+
+    test('returns the created time of the pathname as the crawl time', () => {
+        for (const pathname of pathnames) {
+            expect(response).toContainEqual(
+                expect.objectContaining({
+                    crawledAt: pathname.createdAt
+                })
+            );
+        }
+    });
+});
