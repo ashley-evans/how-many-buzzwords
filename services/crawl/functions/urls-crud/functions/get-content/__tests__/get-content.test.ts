880e118b6cdb1d629dc50331f3b1eb352d9d39e6
Ashley Evans <ashleyev@hotmail.co.uk>
2022-02-21 00:09:40 +0000

    Add get-content.ts to build get-content lambda

---
+++ services/crawl/functions/urls-crud/functions/get-content/__tests__/get-content.test.ts
@@ -0,0 +1,16 @@
+import { mock } from 'jest-mock-extended';
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+jest.mock('buzzword-aws-crawl-urls-repository-library');
+
+import { handler } from '../get-content';
+
+const mockEvent = mock<APIGatewayProxyEvent>();
+
+test('throws error if content bucket name is undefined', async () => {
+    delete process.env.CONTENT_BUCKET_NAME;
+
+    await expect(handler(mockEvent)).rejects.toThrow(
+        new Error('Content Bucket Name has not been set.')
+    );
+});
