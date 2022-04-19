85c1fc580c2a9c0963d5d1cdc32736d2fd7d93e8
Ashley Evans <ashleyev@hotmail.co.uk>
2022-04-19 08:56:56 +0100

    Add WebSocketClientFactory and AWS implementation to create a given web socket client configured to a URL

---
+++ services/keyphrase/libs/web-socket-client-library/factories/__tests__/AWSWebSocketClientFactory.test.ts
@@ -0,0 +1,12 @@
+import AWSWebSocketClient from "../../clients/AWSWebSocketClient";
+import AWSWebSocketClientFactory from "../AWSWebSocketClientFactory";
+
+const factory = new AWSWebSocketClientFactory();
+
+test("given a valid URL creates a web socket client for that URL", () => {
+    const url = new URL("https://www.example.com/");
+
+    const client = factory.createClient(url);
+
+    expect(client).toBeInstanceOf(AWSWebSocketClient);
+});
