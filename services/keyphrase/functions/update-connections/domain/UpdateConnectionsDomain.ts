9acb174a4703237c9d1eb6e8e4b0b1b6a15e2c6d
Ashley Evans <ashleyev@hotmail.co.uk>
2022-05-22 21:05:58 +0100

    Update UpdateConnectionsDomain to handle multiple listening clients from different callback URLs

--- services/keyphrase/functions/update-connections/domain/UpdateConnectionsDomain.ts
+++ services/keyphrase/functions/update-connections/domain/UpdateConnectionsDomain.ts
@@ -1,5 +1,10 @@
-import { WebSocketClientFactory } from "buzzword-aws-web-socket-client-library";
+import { chain } from "lodash";
+import {
+    WebSocketClient,
+    WebSocketClientFactory,
+} from "buzzword-aws-web-socket-client-library";
 import { ActiveConnectionsRepositoryPort } from "buzzword-aws-active-connections-repository-library";
+import { PathnameOccurrences } from "buzzword-aws-keyphrase-repository-library";
 
 import {
     BaseURLOccurrences,
@@ -7,23 +12,66 @@ import {
 } from "../ports/UpdateConnectionsPort";
 
 class UpdateConnectionsDomain implements UpdateConnectionsPort {
+    private clients: Map<URL, WebSocketClient>;
+
     constructor(
         private factory: WebSocketClientFactory,
         private repository: ActiveConnectionsRepositoryPort
-    ) {}
+    ) {
+        this.clients = new Map();
+    }
 
     async updateExistingConnections(
         occurrences: BaseURLOccurrences[]
     ): Promise<BaseURLOccurrences[]> {
-        const uniqueBaseURLs = [
-            ...new Set(occurrences.map((occurrence) => occurrence.baseURL)),
-        ];
+        const groupedOccurrences = this.groupOccurrences(occurrences);
 
-        for (const baseURL of uniqueBaseURLs) {
-            this.repository.getListeningConnections(baseURL);
+        for (const occurrrenceGroup of groupedOccurrences) {
+            const connections = await this.repository.getListeningConnections(
+                occurrrenceGroup.baseURL
+            );
+
+            for (const connection of connections) {
+                const client = this.getClient(connection.callbackURL);
+                await client.sendData(
+                    JSON.stringify(occurrrenceGroup.occurrences),
+                    connection.connectionID
+                );
+            }
         }
 
         return [];
+    }
+
+    private groupOccurrences(
+        occurrences: BaseURLOccurrences[]
+    ): { baseURL: string; occurrences: PathnameOccurrences[] }[] {
+        return chain(occurrences)
+            .groupBy((occurrence) => occurrence.baseURL)
+            .map((groupedOccurrences, baseURL) => ({
+                baseURL,
+                occurrences: groupedOccurrences.map((occurrence) => {
+                    const mapped: PathnameOccurrences = {
+                        pathname: occurrence.pathname,
+                        keyphrase: occurrence.keyphrase,
+                        occurrences: occurrence.occurrences,
+                    };
+
+                    return mapped;
+                }),
+            }))
+            .value();
+    }
+
+    private getClient(callbackURL: URL): WebSocketClient {
+        const client = this.clients.get(callbackURL);
+        if (client) {
+            return client;
+        }
+
+        const newClient = this.factory.createClient(callbackURL);
+        this.clients.set(callbackURL, newClient);
+        return newClient;
     }
 }
 
--- services/keyphrase/functions/update-connections/domain/__tests__/UpdateConnectionsDomain.test.ts
+++ services/keyphrase/functions/update-connections/domain/__tests__/UpdateConnectionsDomain.test.ts
@@ -3,38 +3,51 @@ import {
     WebSocketClientFactory,
     WebSocketClient,
 } from "buzzword-aws-web-socket-client-library";
-import { ActiveConnectionsRepositoryPort } from "buzzword-aws-active-connections-repository-library";
+import {
+    ActiveConnectionsRepositoryPort,
+    Connection,
+} from "buzzword-aws-active-connections-repository-library";
 
 import UpdateConnectionsDomain from "../UpdateConnectionsDomain";
 import { BaseURLOccurrences } from "../../ports/UpdateConnectionsPort";
+import { PathnameOccurrences } from "buzzword-aws-keyphrase-repository-library";
 
 const mockClientFactory = mock<WebSocketClientFactory>();
 const mockClient = mock<WebSocketClient>();
 const mockRepository = mock<ActiveConnectionsRepositoryPort>();
 
-const domain = new UpdateConnectionsDomain(mockClientFactory, mockRepository);
-
 const BASE_URL = "www.example.com";
 const OTHER_BASE_URL = "www.other.com";
-const PATHNAME = "/example";
-const OTHER_PATHNAME = "/other";
-const KEYPHRASE = "test";
-const OTHER_KEYPHRASE = "wibble";
-const OCCURRENCES = 43;
-const OTHER_OCCURRENCES = 13;
+const CALLBACK_URL = new URL("https://www.example-callback.com/");
+const OTHER_CALLBACK_URL = new URL("https://www.other-callback.com/");
 
-function createOccurrence(
+function createOccurrences(
     baseURL: string,
-    pathname: string,
-    keyphrase: string,
-    occurrences: number
-): BaseURLOccurrences {
-    return {
-        baseURL,
-        pathname,
-        keyphrase,
-        occurrences,
-    };
+    amount: number
+): BaseURLOccurrences[] {
+    const occurrences: BaseURLOccurrences[] = [];
+    for (let i = 0; i < amount; i++) {
+        occurrences.push({
+            baseURL,
+            pathname: `/path-${i}`,
+            keyphrase: `keyphrase-${i}`,
+            occurrences: i,
+        });
+    }
+
+    return occurrences;
+}
+
+function createConnections(callbackURL: URL, amount: number): Connection[] {
+    const connections: Connection[] = [];
+    for (let i = 0; i < amount; i++) {
+        connections.push({
+            connectionID: `test_connection_id_${i}`,
+            callbackURL,
+        });
+    }
+
+    return connections;
 }
 
 function getUniqueBaseURLs(occurrences: BaseURLOccurrences[]): string[] {
@@ -42,57 +55,47 @@ function getUniqueBaseURLs(occurrences: BaseURLOccurrences[]): string[] {
     return [...new Set(baseURLs)];
 }
 
+function getUniqueCallbackURLs(connections: Connection[]): URL[] {
+    const callbackURLs = connections.map(
+        (connection) => connection.callbackURL
+    );
+    return [...new Set(callbackURLs)];
+}
+
+function getPathnameOccurrences(
+    baseURL: string,
+    occurrences: BaseURLOccurrences[]
+): PathnameOccurrences[] {
+    return occurrences
+        .filter((occurrence) => occurrence.baseURL == baseURL)
+        .map((filtered) => ({
+            pathname: filtered.pathname,
+            keyphrase: filtered.keyphrase,
+            occurrences: filtered.occurrences,
+        }));
+}
+
 describe.each([
     [
-        "a single keyphrase occurrence count for a single base URL and no listening clients",
-        [createOccurrence(BASE_URL, PATHNAME, KEYPHRASE, OCCURRENCES)],
+        "a single keyphrase occurrence count for a single base URL with no listening clients",
+        createOccurrences(BASE_URL, 1),
     ],
     [
         "multiple keyphrase occurrences for a single base URL with no listening clients",
+        createOccurrences(BASE_URL, 2),
+    ],
+    [
+        "a single keyphrase occurrence for multiple base URLs with no listening clients",
         [
-            createOccurrence(BASE_URL, PATHNAME, KEYPHRASE, OCCURRENCES),
-            createOccurrence(
-                BASE_URL,
-                PATHNAME,
-                OTHER_KEYPHRASE,
-                OTHER_OCCURRENCES
-            ),
+            ...createOccurrences(BASE_URL, 1),
+            ...createOccurrences(OTHER_BASE_URL, 1),
         ],
     ],
     [
-        "a single keyphrase occurrence for multiple base URLs and no listening clients",
+        "multiple keyphrase occurrences for multiple base URLs with no listening clients",
         [
-            createOccurrence(BASE_URL, PATHNAME, KEYPHRASE, OCCURRENCES),
-            createOccurrence(
-                OTHER_BASE_URL,
-                OTHER_PATHNAME,
-                OTHER_KEYPHRASE,
-                OTHER_OCCURRENCES
-            ),
-        ],
-    ],
-    [
-        "multiple keyphrase occurrences for multiple base URLs and no listening clients",
-        [
-            createOccurrence(BASE_URL, PATHNAME, KEYPHRASE, OCCURRENCES),
-            createOccurrence(
-                BASE_URL,
-                OTHER_PATHNAME,
-                OTHER_KEYPHRASE,
-                OTHER_OCCURRENCES
-            ),
-            createOccurrence(
-                OTHER_BASE_URL,
-                OTHER_PATHNAME,
-                OTHER_KEYPHRASE,
-                OTHER_OCCURRENCES
-            ),
-            createOccurrence(
-                OTHER_BASE_URL,
-                PATHNAME,
-                KEYPHRASE,
-                OTHER_OCCURRENCES
-            ),
+            ...createOccurrences(BASE_URL, 2),
+            ...createOccurrences(OTHER_BASE_URL, 2),
         ],
     ],
 ])(
@@ -105,6 +108,11 @@ describe.each([
         beforeAll(async () => {
             jest.resetAllMocks();
             mockClientFactory.createClient.mockReturnValue(mockClient);
+            mockRepository.getListeningConnections.mockResolvedValue([]);
+            const domain = new UpdateConnectionsDomain(
+                mockClientFactory,
+                mockRepository
+            );
 
             response = await domain.updateExistingConnections(occurrences);
         });
@@ -126,6 +134,103 @@ describe.each([
 
         test("does not call any existing web socket client to send the new occurrences", () => {
             expect(mockClient.sendData).not.toHaveBeenCalled();
+        });
+
+        test("returns no failures", () => {
+            expect(response).toBeDefined();
+            expect(response).toHaveLength(0);
+        });
+    }
+);
+
+describe.each([
+    [
+        "a single keyphrase occurrence for a single base URL with a single listening client",
+        1,
+        createConnections(CALLBACK_URL, 1),
+    ],
+    [
+        "a single keyphrase occurrence for a single base URL with multiple listening clients",
+        1,
+        createConnections(CALLBACK_URL, 2),
+    ],
+    [
+        "a single keyphrase occurrences for a single base URL with a single listening client",
+        1,
+        createConnections(CALLBACK_URL, 2),
+    ],
+    [
+        "multiple keyphrase occurrences for a single base URL with multiple listening clients",
+        2,
+        createConnections(CALLBACK_URL, 2),
+    ],
+    [
+        "multiple keyphrase occurrences for a single base URL with multiple listening clients with different callback URLs",
+        2,
+        [
+            ...createConnections(CALLBACK_URL, 1),
+            ...createConnections(OTHER_CALLBACK_URL, 1),
+        ],
+    ],
+])(
+    "given %s",
+    (
+        message: string,
+        numberOfOccurrences: number,
+        connections: Connection[]
+    ) => {
+        const occurrences = createOccurrences(BASE_URL, numberOfOccurrences);
+        const uniqueCallbackURLs = getUniqueCallbackURLs(connections);
+
+        let response: BaseURLOccurrences[];
+
+        beforeAll(async () => {
+            jest.resetAllMocks();
+            mockClientFactory.createClient.mockReturnValue(mockClient);
+            mockRepository.getListeningConnections.mockResolvedValue(
+                connections
+            );
+            const domain = new UpdateConnectionsDomain(
+                mockClientFactory,
+                mockRepository
+            );
+
+            response = await domain.updateExistingConnections(occurrences);
+        });
+
+        test("calls the active connections repository for each unique base URL", () => {
+            expect(
+                mockRepository.getListeningConnections
+            ).toHaveBeenCalledTimes(1);
+            expect(mockRepository.getListeningConnections).toHaveBeenCalledWith(
+                BASE_URL
+            );
+        });
+
+        test("creates a client for each unique callback URL", () => {
+            expect(mockClientFactory.createClient).toHaveBeenCalledTimes(
+                uniqueCallbackURLs.length
+            );
+            for (const callbackURL of uniqueCallbackURLs) {
+                expect(mockClientFactory.createClient).toHaveBeenCalledWith(
+                    callbackURL
+                );
+            }
+        });
+
+        test("sends all keyphrase occurrences to each listening client", () => {
+            const expectedOccurences: PathnameOccurrences[] =
+                getPathnameOccurrences(BASE_URL, occurrences);
+
+            expect(mockClient.sendData).toHaveBeenCalledTimes(
+                connections.length
+            );
+            for (const connection of connections) {
+                expect(mockClient.sendData).toHaveBeenCalledWith(
+                    JSON.stringify(expectedOccurences),
+                    connection.connectionID
+                );
+            }
         });
 
         test("returns no failures", () => {
