bc3ac395d16e58fe143b4f78cc29761b67b7a8ff
Ashley Evans <ashleyev@hotmail.co.uk>
2022-05-22 14:40:06 +0100

    Add UpdateConnectionsPort definition for updating existing connections functionality

---
+++ services/keyphrase/functions/update-connections/ports/UpdateConnectionsPort.ts
@@ -0,0 +1,14 @@
+type BaseURLOccurrences = {
+    baseURL: string;
+    pathname: string;
+    keyphrase: string;
+    occurrences: number;
+};
+
+interface UpdateConnectionsPort {
+    updateExistingConnections(
+        occurrences: BaseURLOccurrences[]
+    ): Promise<BaseURLOccurrences>;
+}
+
+export { BaseURLOccurrences, UpdateConnectionsPort };
