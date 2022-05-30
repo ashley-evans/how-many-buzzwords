4555312f9b165601ddd137902c9218a2eae1c599
Ashley Evans <ashleyev@hotmail.co.uk>
2022-05-30 17:23:22 +0100

    Add update-connections to build required components to handle incoming dyanmodb stream events
    Update update-connections package.json to remove redundant dependencies

---
+++ services/keyphrase/functions/update-connections/__tests__/update-connections.test.ts
@@ -0,0 +1,12 @@
+jest.mock("buzzword-aws-active-connections-repository-library");
+
+test("throws error if table name is undefined", async () => {
+    delete process.env.TABLE_NAME;
+
+    expect.assertions(1);
+    await expect(async () => {
+        await import("../update-connections");
+    }).rejects.toThrow(
+        new Error("Active Connections table name has not been set.")
+    );
+});
---
+++ services/keyphrase/functions/update-connections/update-connections.ts
@@ -0,0 +1,28 @@
+import { DynamoDBStreamEvent, SQSBatchResponse } from "aws-lambda";
+import { AWSWebSocketClientFactory } from "buzzword-aws-web-socket-client-library";
+import {
+    ActiveConnectionsRepository,
+    ActiveConnectionsRepositoryPort,
+} from "buzzword-aws-active-connections-repository-library";
+
+import UpdateConnectionsDomain from "./domain/UpdateConnectionsDomain";
+import UpdateConnectionsStreamAdapter from "./adapters/UpdateConnectionsStreamAdapter";
+
+function createRepository(): ActiveConnectionsRepositoryPort {
+    if (!process.env.TABLE_NAME) {
+        throw new Error("Active Connections table name has not been set.");
+    }
+
+    return new ActiveConnectionsRepository(process.env.TABLE_NAME);
+}
+
+const repository = createRepository();
+const factory = new AWSWebSocketClientFactory();
+const domain = new UpdateConnectionsDomain(factory, repository);
+const adapter = new UpdateConnectionsStreamAdapter(domain);
+
+async function handler(event: DynamoDBStreamEvent): Promise<SQSBatchResponse> {
+    return adapter.handleEvent(event);
+}
+
+export { handler };
