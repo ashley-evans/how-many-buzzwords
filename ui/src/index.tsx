816822a013c2b2334084b77cbd95040ee26b3616
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-01 12:20:48 +0100

    Create App.tsx to seperate entrypoint from React components

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-    <div>How many buzzwords - React</div>,
-    document.getElementById("root")
-);
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
