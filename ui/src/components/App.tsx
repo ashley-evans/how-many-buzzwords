7a873d720b7c5c70fd95188494fff257ca977228
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-01 12:49:10 +0100

    Update App.tsx to test pipeline

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class App extends Component {
     render() {
-        return <div>How many buzzwords - React</div>;
+        return <div>How many buzzwords</div>;
     }
 }
 
