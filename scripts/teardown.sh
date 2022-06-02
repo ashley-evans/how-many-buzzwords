a8ad237a56f777944cb3e33efccea20324ae4d75
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-02 11:32:22 +0100

    Update teardown-stack script to remove all content from S3 buckets before stack deletion
    Update teardown to add teardown for UI stack and remove duplicate S3 content removal

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -16,17 +16,6 @@ getstackname() {
     echo $stack_name
 }
 
-deletecrawlcontent() {
-    echo "Emptying crawled content bucket"
-
-    content_bucket_physical_id=$(aws cloudformation describe-stack-resource \
-        --stack-name "$1" \
-        --logical-resource-id CrawlContentBucket \
-        | jq -r .StackResourceDetail.PhysicalResourceId)
-
-    aws s3 rm "s3://$content_bucket_physical_id/" --recursive
-}
-
 deletestack() {
     $script_dir/helpers/teardown-stack.sh -s $1
 }
@@ -65,6 +54,17 @@ esac
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 root_dir="$( dirname "$script_dir")"
 
+ui_config_path="$root_dir/ui/samconfig.toml"
+if [ ! -f $ui_config_path ]; then
+    echo "Error: Cannot find UI config file." >&2
+    exit 1
+else
+    echo "Deleting UI stack..."
+    stack_name=$(getstackname $ui_config_path)
+
+    deletestack $stack_name
+fi
+
 keyphrase_config_path="$root_dir/services/keyphrase/samconfig.toml"
 if [ ! -f $keyphrase_config_path ]; then
     echo "Error: Cannot find keyphrase service config file." >&2
@@ -84,6 +84,5 @@ else
     echo "Deleting crawl service..."
     stack_name=$(getstackname $crawl_config_path)
 
-    deletecrawlcontent $stack_name
     deletestack $stack_name
 fi
