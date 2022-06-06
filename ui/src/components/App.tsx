81a5278b27e6b018b065287ec3da18cd439b21c2
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-06 18:53:44 +0100

    Update App.tsx with basic URL form rendering

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
 
-class App extends Component {
+type AppState = {
+    baseURL: string;
+};
+
+class App extends Component<unknown, AppState> {
+    state: AppState = {
+        baseURL: "",
+    };
+
+    handleURLChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        this.setState({ baseURL: event.target.value });
+    };
+
+    handleCrawlSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+        alert("A URL was submitted: " + this.state.baseURL);
+    };
+
     render() {
-        return <div>How many buzzwords</div>;
+        return (
+            <div>
+                <h1>How many buzzwords</h1>
+                <form onSubmit={this.handleCrawlSubmit}>
+                    <label>
+                        URL:
+                        <input
+                            type="text"
+                            value={this.state.baseURL}
+                            onChange={this.handleURLChange}
+                        />
+                    </label>
+                    <input type="submit" value="Search!" />
+                </form>
+            </div>
+        );
     }
 }
 
