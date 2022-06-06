76859b0e236dc58881c4b3489d80698a4cab8a9e
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-06 21:37:46 +0100

    Update App.tsx to display processing message after a valid URL is entered

--- ui/src/components/__tests__/App.test.tsx
+++ ui/src/components/__tests__/App.test.tsx
@@ -3,42 +3,39 @@ import { fireEvent, render } from "@testing-library/react";
 
 import App from "../App";
 
+const APPLICATION_TITLE = "How many buzzwords";
+const URL_INPUT_LABEL = "URL:";
+const SEARCH_BUTTON_TEXT = "Search!";
+
+const VALID_URL = "http://www.example.com/";
+
 describe("field rendering", () => {
     test("displays the title of the site in a header", () => {
-        const expectedTitle = "How many buzzwords";
-
         const { getByRole } = render(<App />);
 
         expect(
-            getByRole("heading", { name: expectedTitle })
+            getByRole("heading", { name: APPLICATION_TITLE })
         ).toBeInTheDocument();
     });
 
     test("displays a URL textbox with an appropriate label", () => {
-        const expectedLabel = "URL:";
-
         const { getByRole } = render(<App />);
 
         expect(
-            getByRole("textbox", { name: expectedLabel })
+            getByRole("textbox", { name: URL_INPUT_LABEL })
         ).toBeInTheDocument();
     });
 
     test("displays a search button", () => {
-        const expectedButtonText = "Search!";
-
         const { getByRole } = render(<App />);
 
         expect(
-            getByRole("button", { name: expectedButtonText })
+            getByRole("button", { name: SEARCH_BUTTON_TEXT })
         ).toBeInTheDocument();
     });
 });
 
 describe("input validation", () => {
-    const URLInputBoxLabel = "URL:";
-    const searchButtonText = "Search!";
-
     test.each([
         ["empty input", "", "Please enter a URL."],
         ["a numeric value", "123", "Please enter a valid URL"],
@@ -47,26 +44,26 @@ describe("input validation", () => {
         "provides error message given %s",
         (message: string, input: string, expectedError: string) => {
             const { getByRole } = render(<App />);
-            fireEvent.input(getByRole("textbox", { name: URLInputBoxLabel }), {
+            fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
                 target: { value: input },
             });
-            fireEvent.submit(getByRole("button", { name: searchButtonText }));
+            fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
 
             expect(getByRole("alert")).toHaveTextContent(expectedError);
         }
     );
 
     test.each([
-        ["with a protocol", "https://www.example.com/"],
+        ["with a protocol", VALID_URL],
         ["with no protocol", "www.example.com"],
     ])(
         "does not provide an error message given a valid URL %s",
         (message: string, input: string) => {
             const { getByRole, queryByRole } = render(<App />);
-            fireEvent.input(getByRole("textbox", { name: URLInputBoxLabel }), {
+            fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
                 target: { value: input },
             });
-            fireEvent.submit(getByRole("button", { name: searchButtonText }));
+            fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
 
             expect(queryByRole("alert")).toBeNull();
         }
@@ -74,14 +71,51 @@ describe("input validation", () => {
 
     test("clears error message after changing input text", () => {
         const { getByRole, queryByRole } = render(<App />);
-        fireEvent.input(getByRole("textbox", { name: URLInputBoxLabel }), {
+        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
             target: { value: "" },
         });
-        fireEvent.submit(getByRole("button", { name: searchButtonText }));
-        fireEvent.input(getByRole("textbox", { name: URLInputBoxLabel }), {
-            target: { value: "https://www.example.com/" },
+        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
+        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
+            target: { value: VALID_URL },
         });
 
         expect(queryByRole("alert")).toBeNull();
+    });
+});
+
+describe("process screen rendering", () => {
+    test("renders the title of the site while crawling", () => {
+        const { getByRole } = render(<App />);
+        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
+            target: { value: VALID_URL },
+        });
+        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
+
+        expect(
+            getByRole("heading", { name: APPLICATION_TITLE })
+        ).toBeInTheDocument();
+    });
+
+    test("does not render the URL input form while crawling", () => {
+        const { getByRole, queryByRole } = render(<App />);
+        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
+            target: { value: VALID_URL },
+        });
+        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
+
+        expect(queryByRole("textbox", { name: URL_INPUT_LABEL })).toBeNull();
+        expect(queryByRole("button", { name: SEARCH_BUTTON_TEXT })).toBeNull();
+    });
+
+    test("renders loading message while crawling", () => {
+        const expectedMessage = "Crawling...";
+
+        const { getByRole, getByText } = render(<App />);
+        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
+            target: { value: VALID_URL },
+        });
+        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
+
+        expect(getByText(expectedMessage)).toBeInTheDocument();
     });
 });
