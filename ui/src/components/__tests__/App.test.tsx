1f03d5a1a7e6c3c1d7c07563093b5b632a79ed4f
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-06 18:53:44 +0100

    Update App.tsx with basic URL form rendering

--- ui/src/components/__tests__/App.test.tsx
+++ ui/src/components/__tests__/App.test.tsx
@@ -3,10 +3,34 @@ import { render } from "@testing-library/react";
 
 import App from "../App";
 
-test("renders a message", () => {
-    const expectedMessage = "How many buzzwords";
+describe("field rendering", () => {
+    test("displays the title of the site in a header", () => {
+        const expectedTitle = "How many buzzwords";
 
-    const { getByText } = render(<App />);
+        const { getByRole } = render(<App />);
 
-    expect(getByText(expectedMessage)).toBeInTheDocument();
+        expect(
+            getByRole("heading", { name: expectedTitle })
+        ).toBeInTheDocument();
+    });
+
+    test("displays a URL textbox with an appropriate label", () => {
+        const expectedLabel = "URL:";
+
+        const { getByRole } = render(<App />);
+
+        expect(
+            getByRole("textbox", { name: expectedLabel })
+        ).toBeInTheDocument();
+    });
+
+    test("displays a search button", () => {
+        const expectedButtonText = "Search!";
+
+        const { getByRole } = render(<App />);
+
+        expect(
+            getByRole("button", { name: expectedButtonText })
+        ).toBeInTheDocument();
+    });
 });
