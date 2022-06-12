e8323727bf5765c254ab8536168fd8e0086ae032
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-12 20:37:01 +0100

    Update index.tsx to create keyphrase service client factory and provide to root app component

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import CrawlServiceAxiosClient from "./clients/CrawlServiceAxiosClient";
+import KeyphraseServiceWSClientFactory from "./clients/factories/KeyphraseServiceWSClientFactory";
 
 import App from "./components/App";
 
@@ -9,9 +10,21 @@ const crawlServiceEndpoint = new URL(
 );
 const crawlServiceClient = new CrawlServiceAxiosClient(crawlServiceEndpoint);
 
+const keyphraseServiceEndpoint = new URL(
+    "wss://peeam3kix4.execute-api.eu-west-2.amazonaws.com/$default/"
+);
+const keyphraseServiceClientFactory = new KeyphraseServiceWSClientFactory(
+    keyphraseServiceEndpoint
+);
+
 const container = document.getElementById("root");
 if (!container) {
     throw new Error("No root element found!");
 }
 const root = createRoot(container);
-root.render(<App crawlServiceClient={crawlServiceClient} />);
+root.render(
+    <App
+        crawlServiceClient={crawlServiceClient}
+        keyphraseServiceClientFactory={keyphraseServiceClientFactory}
+    />
+);
