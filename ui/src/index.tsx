a137bdafa727fe25f8ee96ea1a2a8d1d0a84a37a
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-12 17:23:56 +0100

    Update index.tsx to create crawl service client and provide to app component

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import CrawlServiceAxiosClient from "./clients/CrawlServiceAxiosClient";
 
 import App from "./components/App";
 
 const crawlServiceEndpoint = new URL(
     "https://vcv01m9s3b.execute-api.eu-west-2.amazonaws.com/"
 );
+const crawlServiceClient = new CrawlServiceAxiosClient(crawlServiceEndpoint);
 
 const container = document.getElementById("root");
 if (!container) {
     throw new Error("No root element found!");
 }
 const root = createRoot(container);
-root.render(<App crawlServiceEndpoint={crawlServiceEndpoint} />);
+root.render(<App crawlServiceClient={crawlServiceClient} />);
