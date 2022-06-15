6667596ab1aa0878638be4351a1a1d10088106ef
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-15 18:38:42 +0100

    Update App.tsx to add results route to render results page

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CrawlServiceClient from "../clients/interfaces/CrawlServiceClient";
 import KeyphraseServiceClientFactory from "../clients/interfaces/KeyphraseServiceClientFactory";
+import Results from "./Results";
 import Search from "./Search";
 
 type AppProps = {
@@ -17,6 +18,16 @@ function App(props: AppProps) {
                     index
                     element={
                         <Search crawlServiceClient={props.crawlServiceClient} />
+                    }
+                />
+                <Route
+                    path="results/:url"
+                    element={
+                        <Results
+                            keyphraseServiceClientFactory={
+                                props.keyphraseServiceClientFactory
+                            }
+                        />
                     }
                 />
             </Routes>
--- ui/src/components/__tests__/App.test.tsx
+++ ui/src/components/__tests__/App.test.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { mock } from "jest-mock-extended";
+import { from } from "rxjs";
 
 import App from "../App";
 import CrawlServiceClient from "../../clients/interfaces/CrawlServiceClient";
 import KeyphraseServiceClientFactory from "../../clients/interfaces/KeyphraseServiceClientFactory";
+import { KeyphraseServiceClient } from "../../clients/interfaces/KeyphraseServiceClient";
 
 const APPLICATION_TITLE = "How many buzzwords";
 
 const mockCrawlClient = mock<CrawlServiceClient>();
 const mockKeyphraseClientFactory = mock<KeyphraseServiceClientFactory>();
+const mockKeyphraseClient = mock<KeyphraseServiceClient>();
 
 beforeEach(() => {
     jest.resetAllMocks();
@@ -56,6 +59,51 @@ describe("navigating to root", () => {
 
         expect(
             getByRole("button", { name: expectedSearchButtonText })
+        ).toBeInTheDocument();
+    });
+});
+
+describe("navigating to results with valid encoded URL", () => {
+    const expectedURL = "https://www.example.com/";
+
+    beforeEach(() => {
+        mockKeyphraseClientFactory.createClient.mockReturnValue(
+            mockKeyphraseClient
+        );
+        mockKeyphraseClient.observeKeyphraseResults.mockReturnValue(from([]));
+
+        window.history.pushState(
+            {},
+            "",
+            `/results/${encodeURIComponent(expectedURL)}`
+        );
+    });
+
+    test("displays the title of the site in a header", () => {
+        const { getByRole } = render(
+            <App
+                crawlServiceClient={mockCrawlClient}
+                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
+            />
+        );
+
+        expect(
+            getByRole("heading", { name: APPLICATION_TITLE })
+        ).toBeInTheDocument();
+    });
+
+    test("renders results header", () => {
+        const expectedHeader = `Results for: ${expectedURL}`;
+
+        const { getByRole } = render(
+            <App
+                crawlServiceClient={mockCrawlClient}
+                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
+            />
+        );
+
+        expect(
+            getByRole("heading", { name: expectedHeader })
         ).toBeInTheDocument();
     });
 });
