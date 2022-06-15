6194bbdeb905323bfe28025a055684e50c0ce0b0
Ashley Evans <ashleyev@hotmail.co.uk>
2022-06-15 18:25:41 +0100

    Update App.tsx to contain router configuration for index page

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,101 +1,27 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CrawlServiceClient from "../clients/interfaces/CrawlServiceClient";
-import { PathnameOccurrences } from "../clients/interfaces/KeyphraseServiceClient";
 import KeyphraseServiceClientFactory from "../clients/interfaces/KeyphraseServiceClientFactory";
-import OccurrenceTable from "./OccurrenceTable";
-import { URLInput } from "./URLInput";
+import Search from "./Search";
 
 type AppProps = {
     crawlServiceClient: CrawlServiceClient;
     keyphraseServiceClientFactory: KeyphraseServiceClientFactory;
 };
 
-type AppState = {
-    baseURL: URL | undefined;
-    crawling: boolean;
-    errorMessage: string;
-    occurrences: PathnameOccurrences[] | undefined;
-};
-
-class App extends Component<AppProps, AppState> {
-    state: AppState = {
-        baseURL: undefined,
-        crawling: false,
-        errorMessage: "",
-        occurrences: undefined,
-    };
-
-    constructor(props: AppProps) {
-        super(props);
-        this.handleURLSubmit = this.handleURLSubmit.bind(this);
-    }
-
-    handleURLSubmit = async (validatedURL: URL) => {
-        this.setState({
-            baseURL: validatedURL,
-            crawling: true,
-            errorMessage: "",
-        });
-        let crawlStarted: boolean | undefined = undefined;
-        try {
-            crawlStarted = await this.props.crawlServiceClient.crawl(
-                validatedURL
-            );
-        } catch {
-            this.setState({
-                errorMessage:
-                    "An error occurred when searching for buzzwords, please try again.",
-            });
-        }
-
-        this.setState({ crawling: false });
-        if (crawlStarted) {
-            this.setState({ occurrences: [] });
-            const client =
-                this.props.keyphraseServiceClientFactory.createClient(
-                    validatedURL
-                );
-            const observable = client.observeKeyphraseResults();
-            observable.subscribe({
-                next: (occurrence) => {
-                    this.setState((state) => {
-                        const currentOccurrences = state.occurrences
-                            ? state.occurrences
-                            : [];
-                        return {
-                            occurrences: [...currentOccurrences, occurrence],
-                        };
-                    });
-                },
-            });
-        } else {
-            this.setState({
-                errorMessage:
-                    "An error occurred when searching for buzzwords, please try again.",
-            });
-        }
-    };
-
-    render() {
-        return (
-            <Fragment>
-                <h1>How many buzzwords</h1>
-                {!this.state.crawling && !this.state.occurrences && (
-                    <URLInput onURLSubmit={this.handleURLSubmit} />
-                )}
-                {this.state.crawling && <p>Crawling...</p>}
-                {this.state.errorMessage != "" && (
-                    <p>{this.state.errorMessage}</p>
-                )}
-                {this.state.occurrences && this.state.baseURL && (
-                    <OccurrenceTable
-                        baseURL={this.state.baseURL}
-                        occurrences={this.state.occurrences}
-                    />
-                )}
-            </Fragment>
-        );
-    }
+function App(props: AppProps) {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    index
+                    element={
+                        <Search crawlServiceClient={props.crawlServiceClient} />
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- ui/src/components/__tests__/App.test.tsx
+++ ui/src/components/__tests__/App.test.tsx
@@ -1,42 +1,21 @@
 import React from "react";
-import {
-    fireEvent,
-    render,
-    waitFor,
-    waitForElementToBeRemoved,
-    within,
-} from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { mock } from "jest-mock-extended";
 
 import App from "../App";
 import CrawlServiceClient from "../../clients/interfaces/CrawlServiceClient";
 import KeyphraseServiceClientFactory from "../../clients/interfaces/KeyphraseServiceClientFactory";
-import {
-    KeyphraseServiceClient,
-    PathnameOccurrences,
-} from "../../clients/interfaces/KeyphraseServiceClient";
-import { from } from "rxjs";
 
 const APPLICATION_TITLE = "How many buzzwords";
-const URL_INPUT_LABEL = "URL:";
-const SEARCH_BUTTON_TEXT = "Search!";
-const CRAWLING_MESSAGE = "Crawling...";
-
-const VALID_URL = "http://www.example.com/";
-const INVALID_URL = "not a valid URL";
 
 const mockCrawlClient = mock<CrawlServiceClient>();
 const mockKeyphraseClientFactory = mock<KeyphraseServiceClientFactory>();
-const mockKeyphraseClient = mock<KeyphraseServiceClient>();
 
 beforeEach(() => {
     jest.resetAllMocks();
-    mockKeyphraseClientFactory.createClient.mockReturnValue(
-        mockKeyphraseClient
-    );
 });
 
-describe("field rendering", () => {
+describe("navigating to root", () => {
     test("displays the title of the site in a header", () => {
         const { getByRole } = render(
             <App
@@ -51,6 +30,8 @@ describe("field rendering", () => {
     });
 
     test("displays a URL textbox with an appropriate label", () => {
+        const expectedURLInputLabel = "URL:";
+
         const { getByRole } = render(
             <App
                 crawlServiceClient={mockCrawlClient}
@@ -59,11 +40,13 @@ describe("field rendering", () => {
         );
 
         expect(
-            getByRole("textbox", { name: URL_INPUT_LABEL })
+            getByRole("textbox", { name: expectedURLInputLabel })
         ).toBeInTheDocument();
     });
 
     test("displays a search button", () => {
+        const expectedSearchButtonText = "Search!";
+
         const { getByRole } = render(
             <App
                 crawlServiceClient={mockCrawlClient}
@@ -72,402 +55,7 @@ describe("field rendering", () => {
         );
 
         expect(
-            getByRole("button", { name: SEARCH_BUTTON_TEXT })
+            getByRole("button", { name: expectedSearchButtonText })
         ).toBeInTheDocument();
     });
-});
-
-describe("process screen rendering", () => {
-    test("renders the title of the site while crawling", async () => {
-        const { getByRole } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-        await waitFor(() => expect(mockCrawlClient.crawl).toHaveBeenCalled());
-
-        expect(
-            getByRole("heading", { name: APPLICATION_TITLE })
-        ).toBeInTheDocument();
-    });
-
-    test("does not render the URL input form while crawling", async () => {
-        const { getByRole, queryByRole } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-        await waitFor(() => expect(mockCrawlClient.crawl).toHaveBeenCalled());
-
-        expect(queryByRole("textbox", { name: URL_INPUT_LABEL })).toBeNull();
-        expect(queryByRole("button", { name: SEARCH_BUTTON_TEXT })).toBeNull();
-    });
-
-    test("renders loading message while crawling", async () => {
-        const { getByRole, getByText } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-
-        await waitFor(() => {
-            expect(getByText(CRAWLING_MESSAGE)).toBeInTheDocument();
-        });
-    });
-});
-
-test("calls crawl service to initiate call given a valid URL", async () => {
-    const { getByRole, queryByText } = render(
-        <App
-            crawlServiceClient={mockCrawlClient}
-            keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-        />
-    );
-    fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-        target: { value: VALID_URL },
-    });
-    fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-    await waitForElementToBeRemoved(() => queryByText(CRAWLING_MESSAGE));
-
-    expect(mockCrawlClient.crawl).toHaveBeenCalledTimes(1);
-    expect(mockCrawlClient.crawl).toHaveBeenCalledWith(new URL(VALID_URL));
-});
-
-test("does not call crawl service to initiate crawl given an invalid URL", () => {
-    const { getByRole } = render(
-        <App
-            crawlServiceClient={mockCrawlClient}
-            keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-        />
-    );
-    fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-        target: { value: INVALID_URL },
-    });
-    fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-
-    expect(mockCrawlClient.crawl).toHaveBeenCalledTimes(0);
-});
-
-test("removes loading message after crawl completes", async () => {
-    const { getByRole, queryByText } = render(
-        <App
-            crawlServiceClient={mockCrawlClient}
-            keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-        />
-    );
-    fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-        target: { value: VALID_URL },
-    });
-    fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-
-    await waitFor(() => {
-        expect(queryByText(CRAWLING_MESSAGE)).not.toBeInTheDocument();
-    });
-});
-
-describe("crawl error rendering", () => {
-    test("renders an error message if an unhandled exception is thrown by the crawl client", async () => {
-        const expectedErrorMessage =
-            "An error occurred when searching for buzzwords, please try again.";
-        mockCrawlClient.crawl.mockRejectedValue(new Error());
-        const { getByRole, queryByText } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-
-        await waitFor(() => {
-            expect(queryByText(expectedErrorMessage)).toBeInTheDocument();
-        });
-    });
-
-    test("renders an error message if the crawl service returns an error", async () => {
-        const expectedErrorMessage =
-            "An error occurred when searching for buzzwords, please try again.";
-        mockCrawlClient.crawl.mockResolvedValue(false);
-        const { getByRole, queryByText } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-
-        await waitFor(() => {
-            expect(queryByText(expectedErrorMessage)).toBeInTheDocument();
-        });
-    });
-
-    test("clears error message following re-crawl", async () => {
-        const expectedErrorMessage =
-            "An error occurred when searching for buzzwords, please try again.";
-        mockCrawlClient.crawl.mockRejectedValueOnce(new Error());
-        const { getByRole, queryByText } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-        await waitFor(() => {
-            expect(queryByText(expectedErrorMessage)).toBeInTheDocument();
-        });
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-        await waitFor(() => {
-            expect(queryByText(expectedErrorMessage)).not.toBeInTheDocument();
-        });
-    });
-});
-
-describe("web socket connection initiation handling", () => {
-    test("creates a client to listen to keyphrase occurrence results given valid URL submitted", async () => {
-        mockCrawlClient.crawl.mockResolvedValue(true);
-        mockKeyphraseClient.observeKeyphraseResults.mockReturnValue(from([]));
-        const { getByRole, queryByText } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-        await waitForElementToBeRemoved(() => queryByText(CRAWLING_MESSAGE));
-
-        expect(mockKeyphraseClientFactory.createClient).toHaveBeenCalledTimes(
-            1
-        );
-        expect(mockKeyphraseClientFactory.createClient).toHaveBeenCalledWith(
-            new URL(VALID_URL)
-        );
-    });
-
-    test("does not create any keyphrase service clients given an invalid URL submitted", () => {
-        mockCrawlClient.crawl.mockResolvedValue(true);
-        const { getByRole } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: INVALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-
-        expect(mockKeyphraseClientFactory.createClient).toHaveBeenCalledTimes(
-            0
-        );
-    });
-
-    test("does not create a keyphrase service client if the crawl fails to start", async () => {
-        mockCrawlClient.crawl.mockResolvedValue(false);
-        const { getByRole, queryByText } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-        await waitForElementToBeRemoved(() => queryByText(CRAWLING_MESSAGE));
-
-        expect(mockKeyphraseClientFactory.createClient).toHaveBeenCalledTimes(
-            0
-        );
-    });
-
-    test("does not create a keyphrase service client if the crawl service client throws an unhandled exception", async () => {
-        mockCrawlClient.crawl.mockRejectedValue(new Error());
-        const { getByRole, queryByText } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-        await waitForElementToBeRemoved(() => queryByText(CRAWLING_MESSAGE));
-
-        expect(mockKeyphraseClientFactory.createClient).toHaveBeenCalledTimes(
-            0
-        );
-    });
-});
-
-describe("keyphrase occurrence rendering", () => {
-    const AWAITING_RESULTS_MESSAGE = "Awaiting results...";
-
-    beforeEach(() => {
-        mockCrawlClient.crawl.mockResolvedValue(true);
-        mockKeyphraseClient.observeKeyphraseResults.mockReturnValue(from([]));
-    });
-
-    test("displays results header if crawl initiated successfully", async () => {
-        const expectedHeader = `Results for: ${VALID_URL.toString()}`;
-
-        const { getByRole } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-
-        await waitFor(() =>
-            expect(
-                getByRole("heading", { name: expectedHeader })
-            ).toBeInTheDocument()
-        );
-    });
-
-    test("displays awaiting results message if no keyphrases returned", async () => {
-        const { getByRole, getByText } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-
-        await waitFor(() =>
-            expect(getByText(AWAITING_RESULTS_MESSAGE)).toBeInTheDocument()
-        );
-    });
-
-    test("does not render the URL input form while showing results", async () => {
-        const { getByRole, queryByRole, getByText } = render(
-            <App
-                crawlServiceClient={mockCrawlClient}
-                keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-            />
-        );
-        fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-            target: { value: VALID_URL },
-        });
-        fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-        await waitFor(() =>
-            expect(getByText(AWAITING_RESULTS_MESSAGE)).toBeInTheDocument()
-        );
-
-        expect(queryByRole("textbox", { name: URL_INPUT_LABEL })).toBeNull();
-        expect(queryByRole("button", { name: SEARCH_BUTTON_TEXT })).toBeNull();
-    });
-
-    test.each([
-        [
-            "a single occurrence detail if a single occurrence",
-            [
-                {
-                    pathname: "/test",
-                    keyphrase: "wibble",
-                    occurrences: 15,
-                },
-            ],
-        ],
-        [
-            "multiple occurrence details if multiple occurrences",
-            [
-                {
-                    pathname: "/test",
-                    keyphrase: "wibble",
-                    occurrences: 15,
-                },
-                {
-                    pathname: "/example",
-                    keyphrase: "wobble",
-                    occurrences: 12,
-                },
-            ],
-        ],
-    ])(
-        "renders %s received",
-        async (message: string, expectedOccurrences: PathnameOccurrences[]) => {
-            mockKeyphraseClient.observeKeyphraseResults.mockReturnValue(
-                from(expectedOccurrences)
-            );
-
-            const { getByRole, queryByText } = render(
-                <App
-                    crawlServiceClient={mockCrawlClient}
-                    keyphraseServiceClientFactory={mockKeyphraseClientFactory}
-                />
-            );
-            fireEvent.input(getByRole("textbox", { name: URL_INPUT_LABEL }), {
-                target: { value: VALID_URL },
-            });
-            fireEvent.submit(getByRole("button", { name: SEARCH_BUTTON_TEXT }));
-            await waitFor(() =>
-                expect(queryByText(CRAWLING_MESSAGE)).not.toBeInTheDocument()
-            );
-            await waitFor(() =>
-                expect(
-                    queryByText(AWAITING_RESULTS_MESSAGE)
-                ).not.toBeInTheDocument()
-            );
-            const table = getByRole("grid");
-
-            for (const expectedOccurrence of expectedOccurrences) {
-                await waitFor(() =>
-                    expect(
-                        within(table).getByRole("cell", {
-                            name: expectedOccurrence.pathname,
-                        })
-                    ).toBeInTheDocument()
-                );
-                expect(
-                    within(table).getByRole("cell", {
-                        name: expectedOccurrence.keyphrase,
-                    })
-                ).toBeInTheDocument();
-                expect(
-                    within(table).getByRole("cell", {
-                        name: expectedOccurrence.occurrences.toString(),
-                    })
-                ).toBeInTheDocument();
-            }
-        },
-        2000000
-    );
 });
