cac72e2119e001315fd4d1b8302f8ffc3d280096
Ashley Evans <ashleyev@hotmail.co.uk>
2022-07-19 13:48:10 +0100

    Update ui jest.config.js to ignore all test helper scripts during test execution

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     preset: "ts-jest/presets/js-with-ts",
     testEnvironment: "jsdom",
-    modulePathIgnorePatterns: [".npm-cache", "node_modules"],
+    modulePathIgnorePatterns: [
+        ".npm-cache",
+        "node_modules",
+        "__tests__/helpers",
+    ],
     runner: "groups",
     setupFilesAfterEnv: ["<rootDir>/config/jest-setup.ts"],
 };
