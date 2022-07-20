f2cb0a40c31d9263284a7efba4d70481733a382a
Ashley Evans <ashleyev@hotmail.co.uk>
2022-07-20 17:36:39 +0100

    Update jest.config to handle css imports during jest tests

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -9,4 +9,7 @@ module.exports = {
     ],
     runner: "groups",
     setupFilesAfterEnv: ["<rootDir>/config/jest-setup.ts"],
+    moduleNameMapper: {
+        "\\.(css|less)$": "identity-obj-proxy",
+    },
 };
