2bf906f3b477e3e49a7f0410d0fdfc148141cd73
Ashley Evans <ashleyev@hotmail.co.uk>
2022-07-21 10:10:30 +0100

    Refactor OccurrenceTable to be functional to enable future use of ant hooks

--- ui/src/components/OccurrenceTable.tsx
+++ ui/src/components/OccurrenceTable.tsx
@@ -1,47 +1,43 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import { PathnameOccurrences } from "../clients/interfaces/KeyphraseServiceClient";
 
 type OccurrenceTableProps = {
     occurrences: PathnameOccurrences[];
 };
 
-class OccurrenceTable extends Component<OccurrenceTableProps, unknown> {
-    render(): React.ReactNode {
-        return (
-            <Fragment>
-                {this.props.occurrences.length == 0 && (
-                    <p>Awaiting results...</p>
-                )}
-                {this.props.occurrences.length != 0 && (
-                    <table role="grid">
-                        <thead>
-                            <tr role="row">
-                                <th role="columnheader" scope="col">
-                                    Pathname
-                                </th>
-                                <th role="columnheader" scope="col">
-                                    Keyphrase
-                                </th>
-                                <th role="columnheader" scope="col">
-                                    Occurrences
-                                </th>
+function OccurrenceTable(props: OccurrenceTableProps) {
+    return (
+        <Fragment>
+            {props.occurrences.length == 0 && <p>Awaiting results...</p>}
+            {props.occurrences.length != 0 && (
+                <table role="grid">
+                    <thead>
+                        <tr role="row">
+                            <th role="columnheader" scope="col">
+                                Pathname
+                            </th>
+                            <th role="columnheader" scope="col">
+                                Keyphrase
+                            </th>
+                            <th role="columnheader" scope="col">
+                                Occurrences
+                            </th>
+                        </tr>
+                        {props.occurrences.map((occurrence) => (
+                            <tr
+                                role="row"
+                                key={`${occurrence.pathname}#${occurrence.keyphrase}`}
+                            >
+                                <td>{occurrence.pathname}</td>
+                                <td>{occurrence.keyphrase}</td>
+                                <td>{occurrence.occurrences}</td>
                             </tr>
-                            {this.props.occurrences.map((occurrence) => (
-                                <tr
-                                    role="row"
-                                    key={`${occurrence.pathname}#${occurrence.keyphrase}`}
-                                >
-                                    <td>{occurrence.pathname}</td>
-                                    <td>{occurrence.keyphrase}</td>
-                                    <td>{occurrence.occurrences}</td>
-                                </tr>
-                            ))}
-                        </thead>
-                    </table>
-                )}
-            </Fragment>
-        );
-    }
+                        ))}
+                    </thead>
+                </table>
+            )}
+        </Fragment>
+    );
 }
 
 export default OccurrenceTable;
