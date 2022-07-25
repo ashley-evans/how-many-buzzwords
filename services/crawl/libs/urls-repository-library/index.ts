293526789b12701f94d4be280633cf41fdddf36c
Ashley Evans <ashleyev@hotmail.co.uk>
2022-07-25 15:11:15 +0100

    Update urls-repository-library index.ts to expose crawl status enum

--- services/crawl/libs/urls-repository-library/index.ts
+++ services/crawl/libs/urls-repository-library/index.ts
@@ -1,4 +1,5 @@
+import CrawlStatus from "./enums/CrawlStatus";
 import { Pathname, Repository } from "./ports/Repository";
 import URLsTableRepository from "./adapters/URLsTableRepository";
 
-export { Pathname, Repository, URLsTableRepository };
+export { CrawlStatus, Pathname, Repository, URLsTableRepository };
