3984d60664703893acb9332b5f87305208773fbb
Ashley Evans <ashleyev@hotmail.co.uk>
2022-07-26 19:27:48 +0100

    Update RecentCrawlEventAdapter to remove usage of mutable variables to improve test readability

--- services/crawl/functions/recent-crawl/adapters/__tests__/RecentCrawlEventAdapter.test.ts
+++ services/crawl/functions/recent-crawl/adapters/__tests__/RecentCrawlEventAdapter.test.ts
@@ -2,10 +2,7 @@ import { mock } from "jest-mock-extended";
 import { CrawlStatus } from "buzzword-aws-crawl-urls-repository-library";
 
 import { RecentCrawlEventAdapter } from "../RecentCrawlEventAdapter";
-import {
-    RecentCrawlAdapterResponse,
-    RecentCrawlEvent,
-} from "../../ports/RecentCrawlAdapter";
+import { RecentCrawlEvent } from "../../ports/RecentCrawlAdapter";
 import {
     RecentCrawlPort,
     RecentCrawlResponse,
@@ -29,8 +26,6 @@ test.each([
     ["invalid url (numeric)", "1"],
     ["invalid url", `test ${VALID_URL.toString()}`],
 ])("throws exception given %s", async (message: string, eventURL: string) => {
-    jest.resetAllMocks();
-
     const event = createEvent(eventURL);
 
     expect.assertions(1);
@@ -83,33 +78,39 @@ describe.each([
             crawlTime: new Date(),
         };
 
-        let response: RecentCrawlAdapterResponse;
-
-        beforeAll(async () => {
-            jest.resetAllMocks();
+        beforeEach(() => {
+            mockPort.hasCrawledRecently.mockReset();
             mockPort.hasCrawledRecently.mockResolvedValue(successResponse);
-
-            response = await adapter.hasCrawledRecently(event);
         });
 
-        test("calls domain with provided URL", () => {
+        test("calls domain with provided URL", async () => {
+            await adapter.hasCrawledRecently(event);
+
             expect(mockPort.hasCrawledRecently).toHaveBeenCalledTimes(1);
             expect(mockPort.hasCrawledRecently).toHaveBeenCalledWith(VALID_URL);
         });
 
-        test("returns provided URL's hostname in response", () => {
+        test("returns provided URL's hostname in response", async () => {
+            const response = await adapter.hasCrawledRecently(event);
+
             expect(response.baseURL).toEqual(expectedURLOutput);
         });
 
-        test("returns whether recently crawled in response", () => {
+        test("returns whether recently crawled in response", async () => {
+            const response = await adapter.hasCrawledRecently(event);
+
             expect(response.recentlyCrawled).toEqual(recentlyCrawled);
         });
 
-        test("returns crawl status in response", () => {
+        test("returns crawl status in response", async () => {
+            const response = await adapter.hasCrawledRecently(event);
+
             expect(response.status).toEqual(CrawlStatus.COMPLETE);
         });
 
-        test("returns crawl date time in response", () => {
+        test("returns crawl date time in response", async () => {
+            const response = await adapter.hasCrawledRecently(event);
+
             expect(response.crawlTime).toEqual(successResponse.crawlTime);
         });
     }
@@ -123,33 +124,39 @@ describe.each([
     (message: string, inputURL: URL | string, expectedURLOutput: string) => {
         const event = createEvent(inputURL);
 
-        let response: RecentCrawlAdapterResponse;
-
-        beforeAll(async () => {
-            jest.resetAllMocks();
+        beforeEach(async () => {
+            mockPort.hasCrawledRecently.mockReset();
             mockPort.hasCrawledRecently.mockResolvedValue(undefined);
-
-            response = await adapter.hasCrawledRecently(event);
         });
 
-        test("calls domain with valid URL", () => {
+        test("calls domain with valid URL", async () => {
+            await adapter.hasCrawledRecently(event);
+
             expect(mockPort.hasCrawledRecently).toHaveBeenCalledTimes(1);
             expect(mockPort.hasCrawledRecently).toHaveBeenCalledWith(VALID_URL);
         });
 
-        test("returns provided URL's hostname in response", () => {
+        test("returns provided URL's hostname in response", async () => {
+            const response = await adapter.hasCrawledRecently(event);
+
             expect(response.baseURL).toEqual(expectedURLOutput);
         });
 
-        test("returns not recently crawled in response", () => {
+        test("returns not recently crawled in response", async () => {
+            const response = await adapter.hasCrawledRecently(event);
+
             expect(response.recentlyCrawled).toEqual(false);
         });
 
-        test("returns no status in response", () => {
+        test("returns no status in response", async () => {
+            const response = await adapter.hasCrawledRecently(event);
+
             expect(response.status).toBeUndefined();
         });
 
-        test("returns no crawl datetime in response", () => {
+        test("returns no crawl datetime in response", async () => {
+            const response = await adapter.hasCrawledRecently(event);
+
             expect(response.crawlTime).toBeUndefined();
         });
     }
