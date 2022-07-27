e2b03ba44a3afa4a9803708fa434779b52cab080
Ashley Evans <ashleyev@hotmail.co.uk>
2022-07-27 19:36:28 +0100

    Update UpdateStatusDomain to only send status update to event bus if status is retained successfully

--- services/crawl/functions/update-status/domain/UpdateStatusDomain.ts
+++ services/crawl/functions/update-status/domain/UpdateStatusDomain.ts
@@ -4,19 +4,32 @@ import {
 } from "buzzword-aws-crawl-urls-repository-library";
 
 import UpdateStatusPort from "../ports/UpdateStatusPort";
+import EventClient from "../ports/EventClient";
 
 class UpdateStatusDomain implements UpdateStatusPort {
-    constructor(private repository: Repository) {}
+    constructor(
+        private repository: Repository,
+        private eventClient: EventClient
+    ) {}
 
     async updateCrawlStatus(
         baseURL: URL,
         newStatus: CrawlStatus
     ): Promise<boolean> {
         try {
-            return await this.repository.updateCrawlStatus(
+            const statusUpdated = await this.repository.updateCrawlStatus(
                 baseURL.hostname,
                 newStatus
             );
+
+            if (statusUpdated) {
+                return await this.eventClient.sentStatusUpdate(
+                    baseURL.hostname,
+                    newStatus
+                );
+            }
+
+            return false;
         } catch {
             return false;
         }
