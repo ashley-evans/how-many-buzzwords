75dc9ee2d2b666be128a079181e09fabf40d60b0
Ashley Evans <ashleyev@hotmail.co.uk>
2022-07-31 22:58:56 +0100

    Update root jest.config and tsconfig to include new package folder

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     projects: [
         "<rootDir>/packages/object-validator",
+        "<rootDir>/packages/crawl-client",
         "<rootDir>/services/crawl",
         "<rootDir>/services/keyphrase",
         "<rootDir>/ui",
