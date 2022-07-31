dfb0209dd049665848310cc092c6f5426cfc7204
Ashley Evans <ashleyev@hotmail.co.uk>
2022-07-31 23:10:57 +0100

    Update CrawlHTTPClient.test.ts with failure path tests

--- packages/crawl-client/client/__tests__/CrawlHTTPClient.test.ts
+++ packages/crawl-client/client/__tests__/CrawlHTTPClient.test.ts
@@ -1,4 +1,5 @@
 import fs from "fs-extra";
+import nock from "nock";
 import path from "path";
 
 import { mockURLFromFile } from "../../../../helpers/http-mock";
@@ -13,6 +14,10 @@ const EXPECTED_CONTENT_PATH = "/content";
 const EXPECTED_CONTENT_URL_QUERY_STRING_PARAM = "url";
 
 const client = new CrawlHTTPClient(VALID_SERVICE_URL);
+
+beforeEach(() => {
+    nock.cleanAll();
+});
 
 test("calls GET content on the crawl service with encoded url in query string", async () => {
     const urlMock = mockURLFromFile(
@@ -53,4 +58,42 @@ test("returns received HTML as string", async () => {
     const actual = await client.getContent(VALID_CONTENT_URL);
 
     expect(actual).toEqual(expectedContent);
+});
+
+test("throws an error if a error occurs while obtaining content", async () => {
+    const expectedError = "test error";
+    nock(VALID_SERVICE_URL.toString(), {
+        encodedQueryParams: true,
+    })
+        .get(EXPECTED_CONTENT_PATH)
+        .query({
+            [EXPECTED_CONTENT_URL_QUERY_STRING_PARAM]: encodeURIComponent(
+                VALID_CONTENT_URL.toString()
+            ),
+        })
+        .replyWithError(expectedError);
+
+    expect.assertions(1);
+    await expect(() => client.getContent(VALID_CONTENT_URL)).rejects.toThrow(
+        expectedError
+    );
+});
+
+test("throws an error if a non-200 response is returned while obtaining content", async () => {
+    const expectedError = "Request failed with status code 404";
+    nock(VALID_SERVICE_URL.toString(), {
+        encodedQueryParams: true,
+    })
+        .get(EXPECTED_CONTENT_PATH)
+        .query({
+            [EXPECTED_CONTENT_URL_QUERY_STRING_PARAM]: encodeURIComponent(
+                VALID_CONTENT_URL.toString()
+            ),
+        })
+        .reply(404);
+
+    expect.assertions(1);
+    await expect(() => client.getContent(VALID_CONTENT_URL)).rejects.toThrow(
+        expectedError
+    );
 });
