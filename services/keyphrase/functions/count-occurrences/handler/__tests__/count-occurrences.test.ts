f943064b71fe43f025a3fadb2993cf64e6d1d6f7
Ashley Evans <ashleyev@hotmail.co.uk>
2022-08-11 00:04:34 +0100

    Add count-occurrences handler

---
+++ services/keyphrase/functions/count-occurrences/handler/__tests__/count-occurrences.test.ts
@@ -0,0 +1,25 @@
+jest.mock("buzzword-aws-text-repository-library");
+jest.mock("buzzword-aws-keyphrase-repository-library");
+
+beforeEach(() => {
+    process.env.PARSED_CONTENT_S3_BUCKET_NAME = "test_bucket_name";
+    process.env.KEYPHRASE_TABLE_NAME = "test_keyphrase_table";
+});
+
+test("throws error if parsed content S3 bucket name is undefined", async () => {
+    delete process.env.PARSED_CONTENT_S3_BUCKET_NAME;
+
+    expect.assertions(1);
+    await expect(async () => {
+        await import("../count-occurrences");
+    }).rejects.toThrow(new Error("Parsed Content S3 bucket has not been set."));
+});
+
+test("throws error if keyphrase table name is undefined", async () => {
+    delete process.env.KEYPHRASE_TABLE_NAME;
+
+    expect.assertions(1);
+    await expect(async () => {
+        await import("../count-occurrences");
+    }).rejects.toThrow(new Error("Keyphrases Table Name has not been set."));
+});
---
+++ services/keyphrase/functions/count-occurrences/handler/count-occurrences.ts
@@ -0,0 +1,45 @@
+import {
+    TextRepository,
+    TextS3Repository,
+} from "buzzword-aws-text-repository-library";
+import {
+    Repository,
+    KeyphraseRepository,
+} from "buzzword-aws-keyphrase-repository-library";
+import { CountOccurrencesDomain } from "buzzword-aws-count-occurrences-domain";
+
+import CountOccurrencesEvent from "./types/CountOccurrencesEvent";
+import CountOccurrencesResponse from "./types/CountOccurrencesResponse";
+import { CountOccurrencesEventAdapter } from "./adapter/CountOccurrencesEventAdapter";
+
+function createParsedContentRepository(): TextRepository {
+    if (!process.env.PARSED_CONTENT_S3_BUCKET_NAME) {
+        throw new Error("Parsed Content S3 bucket has not been set.");
+    }
+
+    return new TextS3Repository(process.env.PARSED_CONTENT_S3_BUCKET_NAME);
+}
+
+function createKeyphraseRepository(): Repository {
+    if (!process.env.KEYPHRASE_TABLE_NAME) {
+        throw new Error("Keyphrases Table Name has not been set.");
+    }
+
+    return new KeyphraseRepository(process.env.KEYPHRASE_TABLE_NAME);
+}
+
+const parsedContentRepository = createParsedContentRepository();
+const keyphraseRepository = createKeyphraseRepository();
+const domain = new CountOccurrencesDomain(
+    parsedContentRepository,
+    keyphraseRepository
+);
+const adapter = new CountOccurrencesEventAdapter(domain);
+
+async function handler(
+    event: Partial<CountOccurrencesEvent>
+): Promise<CountOccurrencesResponse> {
+    return adapter.handleEvent(event);
+}
+
+export { handler };
