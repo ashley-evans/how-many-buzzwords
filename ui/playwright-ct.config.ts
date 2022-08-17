fd81f25f8e8b6d0369518312ff341c00f44e2ba1
Ashley Evans <ashleyev@hotmail.co.uk>
2022-08-17 22:59:07 +0100

    Update ui to install playwright browsers
    Update playwright config to give more workers when running CI

--- ui/playwright-ct.config.ts
+++ ui/playwright-ct.config.ts
@@ -8,7 +8,7 @@ const config: PlaywrightTestConfig = {
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 4 : undefined,
     reporter: "list",
     use: {
         trace: "on-first-retry",
