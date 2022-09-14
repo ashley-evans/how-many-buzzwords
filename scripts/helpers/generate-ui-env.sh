b445e863bdf92b12877e85b85f24232af69d6f90
Ashley Evans <ashleyev@hotmail.co.uk>
2022-09-14 23:07:16 +0100

    Update generate-ui-env.sh to obtain ws endpoint from keyphrase service and add to produced env file

--- scripts/helpers/generate-ui-env.sh
+++ scripts/helpers/generate-ui-env.sh
@@ -1,4 +1,5 @@
 crawl_stack="buzzword-crawl-service-dev"
+keyphrase_stack="buzzword-keyphrase-service-dev"
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
@@ -11,3 +12,9 @@ echo "CRAWL_SERVICE_GRAPHQL_ENDPOINT=\"$crawl_graphql_endpoint\"" > .env
 crawl_identity_pool_id=$(echo $crawl_stack_output | jq -r 'select ( .OutputKey == "CrawlIdentityPoolID") | .OutputValue')
 
 echo "CRAWL_IDENTITY_POOL_ID=\"$crawl_identity_pool_id\"" >> .env
+
+keyphrase_stack_output=$($script_dir/fetch-stack-outputs.sh -s $keyphrase_stack)
+
+keyphrase_service_ws_endpoint=$(echo $keyphrase_stack_output | jq -r 'select ( .OutputKey == "WebSocketAPIEndpoint") | .OutputValue')
+
+echo "KEYPHRASE_WS_SERVICE_ENDPOINT=\"$keyphrase_service_ws_endpoint/\$default\"" >> .env
