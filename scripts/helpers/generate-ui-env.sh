4c342f18de885dc88d3dc92d7a506433adab8555
Ashley Evans <ashleyev@hotmail.co.uk>
2022-09-14 23:10:58 +0100

    Update generate-ui-env.sh to add aws region as variable to produced .env

--- scripts/helpers/generate-ui-env.sh
+++ scripts/helpers/generate-ui-env.sh
@@ -1,19 +1,24 @@
+#!/bin/bash
+
 crawl_stack="buzzword-crawl-service-dev"
 keyphrase_stack="buzzword-keyphrase-service-dev"
+region="eu-west-2"
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-crawl_stack_output=$($script_dir/fetch-stack-outputs.sh -s $crawl_stack)
+echo "REGION=\"$region\"" > .env
+
+crawl_stack_output=$($script_dir/fetch-stack-outputs.sh -s $crawl_stack -r $region)
 
 crawl_graphql_endpoint=$(echo $crawl_stack_output | jq -r 'select ( .OutputKey == "CrawlGraphQLAPIEndpoint") | .OutputValue')
 
-echo "CRAWL_SERVICE_GRAPHQL_ENDPOINT=\"$crawl_graphql_endpoint\"" > .env
+echo "CRAWL_SERVICE_GRAPHQL_ENDPOINT=\"$crawl_graphql_endpoint\"" >> .env
 
 crawl_identity_pool_id=$(echo $crawl_stack_output | jq -r 'select ( .OutputKey == "CrawlIdentityPoolID") | .OutputValue')
 
 echo "CRAWL_IDENTITY_POOL_ID=\"$crawl_identity_pool_id\"" >> .env
 
-keyphrase_stack_output=$($script_dir/fetch-stack-outputs.sh -s $keyphrase_stack)
+keyphrase_stack_output=$($script_dir/fetch-stack-outputs.sh -s $keyphrase_stack -r $region)
 
 keyphrase_service_ws_endpoint=$(echo $keyphrase_stack_output | jq -r 'select ( .OutputKey == "WebSocketAPIEndpoint") | .OutputValue')
 
