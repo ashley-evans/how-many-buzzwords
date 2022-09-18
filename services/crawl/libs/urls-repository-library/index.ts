b35c21fb52e119fac0735a85841cfa03a2d3889f
Ashley Evans <ashleyev@hotmail.co.uk>
2022-09-18 21:32:44 +0100

    Update url repository package index.ts to expose constants

--- services/crawl/libs/urls-repository-library/index.ts
+++ services/crawl/libs/urls-repository-library/index.ts
@@ -2,12 +2,14 @@ import CrawlStatus from "./enums/CrawlStatus";
 import { CrawlStatusRecord, Pathname, Repository } from "./ports/Repository";
 import URLsTableRepository from "./adapters/URLsTableRepository";
 import { URLsTableKeyFields } from "./enums/URLsTableFields";
+import URLsTableConstants from "./enums/URLsTableConstants";
 
 export {
     CrawlStatus,
     CrawlStatusRecord,
     Pathname,
     Repository,
+    URLsTableConstants,
     URLsTableRepository,
     URLsTableKeyFields,
 };
