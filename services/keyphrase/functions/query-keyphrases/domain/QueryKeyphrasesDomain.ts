6539833c7ec775ac4805bcec5444b86e2307f7dd
Ashley Evans <ashleyev@hotmail.co.uk>
2022-09-27 22:51:27 +0100

    Update QueryKeyphrasesDomain to handle invalid base URLs and base URLs with protocols/paths

--- services/keyphrase/functions/query-keyphrases/domain/QueryKeyphrasesDomain.ts
+++ services/keyphrase/functions/query-keyphrases/domain/QueryKeyphrasesDomain.ts
@@ -5,14 +5,34 @@ import {
     QueryKeyphrasesPort,
 } from "../ports/QueryKeyphrasesPort";
 
+const INVALID_BASE_URL_ERROR = "Invalid base URL provided.";
+
 class QueryKeyphrasesDomain implements QueryKeyphrasesPort {
     constructor(private repository: Repository) {}
 
     async queryKeyphrases(
         baseURL: string
     ): Promise<PathKeyphraseOccurrences[]> {
-        await this.repository.getOccurrences(baseURL);
+        const validatedURL = this.validateURL(baseURL);
+        await this.repository.getOccurrences(validatedURL.hostname);
         return [];
+    }
+
+    private validateURL(baseURL: string): URL {
+        let url = baseURL;
+        if (!isNaN(parseInt(url))) {
+            throw new Error(INVALID_BASE_URL_ERROR);
+        }
+
+        if (!url.startsWith("https://") && !url.startsWith("http://")) {
+            url = `http://${url}`;
+        }
+
+        try {
+            return new URL(url);
+        } catch {
+            throw new Error(INVALID_BASE_URL_ERROR);
+        }
     }
 }
 
--- services/keyphrase/functions/query-keyphrases/domain/__tests__/QueryKeyphrasesDomain.test.ts
+++ services/keyphrase/functions/query-keyphrases/domain/__tests__/QueryKeyphrasesDomain.test.ts
@@ -12,14 +12,47 @@ beforeEach(() => {
     mockRepository.getOccurrences.mockReset();
 });
 
-test("calls the repository to get the keyphrases related to provided URL", async () => {
-    await domain.queryKeyphrases(EXPECTED_BASE_URL);
+describe.each([
+    ["a number", "1"],
+    ["invalid base URL (space)", "invalid base URL"],
+])(
+    "invalid base URL handling given a number",
+    (message: string, input: string) => {
+        test("throws an invalid input error", async () => {
+            const expectedErrorMessage = "Invalid base URL provided.";
 
-    expect(mockRepository.getOccurrences).toHaveBeenCalledTimes(1);
-    expect(mockRepository.getOccurrences).toHaveBeenCalledWith(
-        EXPECTED_BASE_URL
-    );
-});
+            await expect(domain.queryKeyphrases(input)).rejects.toThrowError(
+                expectedErrorMessage
+            );
+        });
+
+        test("does not call port to get keyphrases", async () => {
+            try {
+                await domain.queryKeyphrases(input);
+            } catch {
+                // Expected Error
+            }
+
+            expect(mockRepository.getOccurrences).not.toHaveBeenCalled();
+        });
+    }
+);
+
+test.each([
+    ["excludes a protocol", EXPECTED_BASE_URL, EXPECTED_BASE_URL],
+    ["includes a protocol", `https://${EXPECTED_BASE_URL}/`, EXPECTED_BASE_URL],
+    ["includes a path", `${EXPECTED_BASE_URL}/test`, EXPECTED_BASE_URL],
+])(
+    "calls the repository to get the keyphrases related to hostname provided base URL that %s",
+    async (message: string, input: string, expectedBaseURL: string) => {
+        await domain.queryKeyphrases(input);
+
+        expect(mockRepository.getOccurrences).toHaveBeenCalledTimes(1);
+        expect(mockRepository.getOccurrences).toHaveBeenCalledWith(
+            expectedBaseURL
+        );
+    }
+);
 
 test("returns an empty array if no keyphrases have been found for provided URL", async () => {
     mockRepository.getOccurrences.mockResolvedValue([]);
