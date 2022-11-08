c413817607224e3eebe7f91536cdfc08a2ef8c1c
Ashley Evans <ashleyev@hotmail.co.uk>
2022-11-08 19:03:23 +0000

    Update QueryKeyphrasesDomain tests to group related tests

--- services/keyphrase/functions/query-keyphrases/domain/__tests__/QueryKeyphrasesDomain.test.ts
+++ services/keyphrase/functions/query-keyphrases/domain/__tests__/QueryKeyphrasesDomain.test.ts
@@ -26,224 +26,235 @@ beforeEach(() => {
     mockGetTotals.mockReset();
 });
 
-describe.each([
-    ["a number", "1"],
-    ["invalid base URL (space)", "invalid base URL"],
-])("invalid base URL handling given %s", (message: string, input: string) => {
-    test("throws an invalid input error", async () => {
-        const expectedErrorMessage = "Invalid URL provided.";
+describe("handles obtaining all keyphrase occurrences for a site", () => {
+    describe.each([
+        ["a number", "1"],
+        ["invalid base URL (space)", "invalid base URL"],
+    ])(
+        "invalid base URL handling given %s",
+        (message: string, input: string) => {
+            test("throws an invalid input error", async () => {
+                const expectedErrorMessage = "Invalid URL provided.";
 
-        expect.assertions(1);
-        await expect(domain.queryKeyphrases(input)).rejects.toThrowError(
-            expectedErrorMessage
-        );
-    });
+                expect.assertions(1);
+                await expect(
+                    domain.queryKeyphrases(input)
+                ).rejects.toThrowError(expectedErrorMessage);
+            });
 
-    test("does not call port to get keyphrases", async () => {
-        try {
-            await domain.queryKeyphrases(input);
-        } catch {
-            // Expected Error
+            test("does not call port to get keyphrases", async () => {
+                try {
+                    await domain.queryKeyphrases(input);
+                } catch {
+                    // Expected Error
+                }
+
+                expect(mockRepository.getOccurrences).not.toHaveBeenCalled();
+            });
         }
+    );
 
-        expect(mockRepository.getOccurrences).not.toHaveBeenCalled();
-    });
-});
+    test.each([
+        ["excludes a protocol", EXPECTED_BASE_URL, EXPECTED_BASE_URL],
+        [
+            "includes a protocol",
+            `https://${EXPECTED_BASE_URL}/`,
+            EXPECTED_BASE_URL,
+        ],
+        ["includes a path", `${EXPECTED_BASE_URL}/test`, EXPECTED_BASE_URL],
+    ])(
+        "calls the repository to get the keyphrases related to hostname provided base URL that %s",
+        async (message: string, input: string, expectedBaseURL: string) => {
+            mockGetOccurrences.mockResolvedValue([]);
 
-test.each([
-    ["excludes a protocol", EXPECTED_BASE_URL, EXPECTED_BASE_URL],
-    ["includes a protocol", `https://${EXPECTED_BASE_URL}/`, EXPECTED_BASE_URL],
-    ["includes a path", `${EXPECTED_BASE_URL}/test`, EXPECTED_BASE_URL],
-])(
-    "calls the repository to get the keyphrases related to hostname provided base URL that %s",
-    async (message: string, input: string, expectedBaseURL: string) => {
+            await domain.queryKeyphrases(input);
+
+            expect(mockRepository.getOccurrences).toHaveBeenCalledTimes(1);
+            expect(mockRepository.getOccurrences).toHaveBeenCalledWith(
+                expectedBaseURL
+            );
+        }
+    );
+
+    test("returns an empty array if no keyphrases have been found on provided base URL", async () => {
         mockGetOccurrences.mockResolvedValue([]);
-
-        await domain.queryKeyphrases(input);
-
-        expect(mockRepository.getOccurrences).toHaveBeenCalledTimes(1);
-        expect(mockRepository.getOccurrences).toHaveBeenCalledWith(
-            expectedBaseURL
-        );
-    }
-);
-
-test("returns an empty array if no keyphrases have been found on provided base URL", async () => {
-    mockGetOccurrences.mockResolvedValue([]);
-
-    const actual = await domain.queryKeyphrases(EXPECTED_BASE_URL);
-
-    expect(actual).toEqual([]);
-});
-
-test.each([
-    [
-        "a single keyphrase has",
-        [
-            {
-                pathname: "/test",
-                keyphrase: "wibble",
-                occurrences: 12,
-                aggregated: true,
-            },
-        ],
-        [{ pathname: "/test", keyphrase: "wibble", occurrences: 12 }],
-    ],
-    [
-        "multiple keyphrases have",
-        [
-            {
-                pathname: "/dyson",
-                keyphrase: "sphere",
-                occurrences: 9,
-                aggregated: true,
-            },
-            {
-                pathname: "/procedural",
-                keyphrase: "code",
-                occurrences: 14,
-                aggregated: false,
-            },
-        ],
-        [
-            {
-                pathname: "/dyson",
-                keyphrase: "sphere",
-                occurrences: 9,
-            },
-            {
-                pathname: "/procedural",
-                keyphrase: "code",
-                occurrences: 14,
-            },
-        ],
-    ],
-])(
-    "returns stored keyphrases if %s been found on provided base URL",
-    async (
-        message: string,
-        stored: PathnameOccurrences[],
-        expected: PathKeyphraseOccurrences[]
-    ) => {
-        mockGetOccurrences.mockResolvedValue(stored);
 
         const actual = await domain.queryKeyphrases(EXPECTED_BASE_URL);
 
-        expect(actual).toEqual(expected);
-    }
-);
+        expect(actual).toEqual([]);
+    });
 
-test("throws an error if an unexpected error occurs getting the keyphrases for a provided base URL", async () => {
-    const expectedError = new Error("test error");
-    mockGetOccurrences.mockRejectedValue(expectedError);
+    test.each([
+        [
+            "a single keyphrase has",
+            [
+                {
+                    pathname: "/test",
+                    keyphrase: "wibble",
+                    occurrences: 12,
+                    aggregated: true,
+                },
+            ],
+            [{ pathname: "/test", keyphrase: "wibble", occurrences: 12 }],
+        ],
+        [
+            "multiple keyphrases have",
+            [
+                {
+                    pathname: "/dyson",
+                    keyphrase: "sphere",
+                    occurrences: 9,
+                    aggregated: true,
+                },
+                {
+                    pathname: "/procedural",
+                    keyphrase: "code",
+                    occurrences: 14,
+                    aggregated: false,
+                },
+            ],
+            [
+                {
+                    pathname: "/dyson",
+                    keyphrase: "sphere",
+                    occurrences: 9,
+                },
+                {
+                    pathname: "/procedural",
+                    keyphrase: "code",
+                    occurrences: 14,
+                },
+            ],
+        ],
+    ])(
+        "returns stored keyphrases if %s been found on provided base URL",
+        async (
+            message: string,
+            stored: PathnameOccurrences[],
+            expected: PathKeyphraseOccurrences[]
+        ) => {
+            mockGetOccurrences.mockResolvedValue(stored);
 
-    expect.assertions(1);
-    await expect(
-        domain.queryKeyphrases(EXPECTED_BASE_URL)
-    ).rejects.toThrowError(expectedError);
-});
+            const actual = await domain.queryKeyphrases(EXPECTED_BASE_URL);
 
-describe("invalid pathname handling given a path with a missing forward slash", () => {
-    const pathname = "test";
-    test("throws an invalid input error", async () => {
-        const expectedErrorMessage = "Invalid URL provided.";
+            expect(actual).toEqual(expected);
+        }
+    );
+
+    test("throws an error if an unexpected error occurs getting the keyphrases for a provided base URL", async () => {
+        const expectedError = new Error("test error");
+        mockGetOccurrences.mockRejectedValue(expectedError);
 
         expect.assertions(1);
         await expect(
-            domain.queryKeyphrases(EXPECTED_BASE_URL, pathname)
-        ).rejects.toThrowError(expectedErrorMessage);
+            domain.queryKeyphrases(EXPECTED_BASE_URL)
+        ).rejects.toThrowError(expectedError);
+    });
+});
+
+describe("handles obtaining all keyphrases occurrences on a given path of a site", () => {
+    describe("invalid pathname handling given a path with a missing forward slash", () => {
+        const pathname = "test";
+        test("throws an invalid input error", async () => {
+            const expectedErrorMessage = "Invalid URL provided.";
+
+            expect.assertions(1);
+            await expect(
+                domain.queryKeyphrases(EXPECTED_BASE_URL, pathname)
+            ).rejects.toThrowError(expectedErrorMessage);
+        });
+
+        test("does not call port to get keyphrases", async () => {
+            try {
+                await domain.queryKeyphrases(EXPECTED_BASE_URL, pathname);
+            } catch {
+                // Expected Error
+            }
+
+            expect(mockRepository.getOccurrences).not.toHaveBeenCalled();
+        });
     });
 
-    test("does not call port to get keyphrases", async () => {
-        try {
-            await domain.queryKeyphrases(EXPECTED_BASE_URL, pathname);
-        } catch {
-            // Expected Error
+    test("calls repository to get keyphrases on given path of URL", async () => {
+        mockGetOccurrences.mockResolvedValue([]);
+
+        await domain.queryKeyphrases(EXPECTED_BASE_URL, EXPECTED_PATH);
+
+        expect(mockRepository.getOccurrences).toHaveBeenCalledTimes(1);
+        expect(mockRepository.getOccurrences).toHaveBeenCalledWith(
+            EXPECTED_BASE_URL,
+            EXPECTED_PATH
+        );
+    });
+
+    test("returns an empty array if no keyphrases stored against path", async () => {
+        mockGetOccurrences.mockResolvedValue([]);
+
+        const actual = await domain.queryKeyphrases(
+            EXPECTED_BASE_URL,
+            EXPECTED_PATH
+        );
+
+        expect(actual).toEqual([]);
+    });
+
+    test.each([
+        [
+            "a single keyphrase has",
+            [
+                {
+                    keyphrase: "wibble",
+                    occurrences: 12,
+                    aggregated: true,
+                },
+            ],
+            [{ keyphrase: "wibble", occurrences: 12 }],
+        ],
+        [
+            "multiple keyphrases have",
+            [
+                {
+                    keyphrase: "sphere",
+                    occurrences: 9,
+                    aggregated: true,
+                },
+                {
+                    keyphrase: "code",
+                    occurrences: 14,
+                    aggregated: false,
+                },
+            ],
+            [
+                {
+                    keyphrase: "sphere",
+                    occurrences: 9,
+                },
+                {
+                    keyphrase: "code",
+                    occurrences: 14,
+                },
+            ],
+        ],
+    ])(
+        "returns stored keyphrases if %s been found on provided path",
+        async (message, stored, expected) => {
+            mockGetOccurrences.mockResolvedValue(stored);
+
+            const actual = await domain.queryKeyphrases(EXPECTED_BASE_URL);
+
+            expect(actual).toEqual(expected);
         }
+    );
 
-        expect(mockRepository.getOccurrences).not.toHaveBeenCalled();
+    test("throws an error if an unexpected error occurs getting the keyphrases for a provided path", async () => {
+        const expectedError = new Error("test error");
+        mockGetOccurrences.mockRejectedValue(expectedError);
+
+        expect.assertions(1);
+        await expect(
+            domain.queryKeyphrases(EXPECTED_BASE_URL, EXPECTED_PATH)
+        ).rejects.toThrowError(expectedError);
     });
-});
-
-test("calls repository to get keyphrases on given path of URL", async () => {
-    mockGetOccurrences.mockResolvedValue([]);
-
-    await domain.queryKeyphrases(EXPECTED_BASE_URL, EXPECTED_PATH);
-
-    expect(mockRepository.getOccurrences).toHaveBeenCalledTimes(1);
-    expect(mockRepository.getOccurrences).toHaveBeenCalledWith(
-        EXPECTED_BASE_URL,
-        EXPECTED_PATH
-    );
-});
-
-test("returns an empty array if no keyphrases stored against path", async () => {
-    mockGetOccurrences.mockResolvedValue([]);
-
-    const actual = await domain.queryKeyphrases(
-        EXPECTED_BASE_URL,
-        EXPECTED_PATH
-    );
-
-    expect(actual).toEqual([]);
-});
-
-test.each([
-    [
-        "a single keyphrase has",
-        [
-            {
-                keyphrase: "wibble",
-                occurrences: 12,
-                aggregated: true,
-            },
-        ],
-        [{ keyphrase: "wibble", occurrences: 12 }],
-    ],
-    [
-        "multiple keyphrases have",
-        [
-            {
-                keyphrase: "sphere",
-                occurrences: 9,
-                aggregated: true,
-            },
-            {
-                keyphrase: "code",
-                occurrences: 14,
-                aggregated: false,
-            },
-        ],
-        [
-            {
-                keyphrase: "sphere",
-                occurrences: 9,
-            },
-            {
-                keyphrase: "code",
-                occurrences: 14,
-            },
-        ],
-    ],
-])(
-    "returns stored keyphrases if %s been found on provided path",
-    async (message, stored, expected) => {
-        mockGetOccurrences.mockResolvedValue(stored);
-
-        const actual = await domain.queryKeyphrases(EXPECTED_BASE_URL);
-
-        expect(actual).toEqual(expected);
-    }
-);
-
-test("throws an error if an unexpected error occurs getting the keyphrases for a provided path", async () => {
-    const expectedError = new Error("test error");
-    mockGetOccurrences.mockRejectedValue(expectedError);
-
-    expect.assertions(1);
-    await expect(
-        domain.queryKeyphrases(EXPECTED_BASE_URL, EXPECTED_PATH)
-    ).rejects.toThrowError(expectedError);
 });
 
 describe("handles obtaining global totals", () => {
