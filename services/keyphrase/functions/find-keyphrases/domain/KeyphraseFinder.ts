3ea15e048af18e5502e6fd819c9b6cd10e76ce39
Ashley Evans <ashleyev@hotmail.co.uk>
2022-11-27 12:56:57 +0000

    Update KeyphraseFinder to dynamic import retext libs

--- services/keyphrase/functions/find-keyphrases/domain/KeyphraseFinder.ts
+++ services/keyphrase/functions/find-keyphrases/domain/KeyphraseFinder.ts
@@ -1,15 +1,6 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-ignore
-import retext from "retext";
-// @ts-ignore
-import retextPos from "retext-pos";
-// @ts-ignore
-import retextKeywords from "retext-keywords";
-// @ts-ignore
-import toString from "nlcst-to-string";
 import { TextRepository } from "buzzword-keyphrase-text-repository-library";
 
-import KeyphrasesPort from "../ports/KeyphrasePort";
+import KeyphrasesPort from "../ports/KeyphrasePort.js";
 
 class KeyphraseFinder implements KeyphrasesPort {
     constructor(private parsedContentRepository: TextRepository) {}
@@ -18,24 +9,35 @@ class KeyphraseFinder implements KeyphrasesPort {
         const uniqueKeyphrases = new Set<string>();
         const content = await this.parsedContentRepository.getPageText(url);
         if (content) {
+            const { retext } = await import("retext");
+            const retextPos = (await import("retext-pos")).default;
+            const retextKeywords = (await import("retext-keywords")).default;
+            const { toString } = await import("nlcst-to-string");
+
             console.log(`Starting keyphrase analysis for: ${url.toString()}`);
             const parsedFile = await retext()
                 .use(retextPos)
                 .use(retextKeywords)
                 .process(content);
-
             console.log(`Completed keyphrase analysis for: ${url.toString()}`);
-            for (const keyword of parsedFile.data.keywords) {
-                const current = toString(keyword.matches[0].node).toLowerCase();
-                uniqueKeyphrases.add(current);
+
+            if (parsedFile.data.keywords) {
+                for (const keyword of parsedFile.data.keywords) {
+                    const current = toString(
+                        keyword.matches[0].node
+                    ).toLowerCase();
+                    uniqueKeyphrases.add(current);
+                }
             }
 
-            for (const keyphrase of parsedFile.data.keyphrases) {
-                const current = keyphrase.matches[0].nodes
-                    .map((node: unknown) => toString(node))
-                    .join("")
-                    .toLowerCase();
-                uniqueKeyphrases.add(current);
+            if (parsedFile.data.keyphrases) {
+                for (const keyphrase of parsedFile.data.keyphrases) {
+                    const current = keyphrase.matches[0].nodes
+                        .map((node) => toString(node))
+                        .join("")
+                        .toLowerCase();
+                    uniqueKeyphrases.add(current);
+                }
             }
         }
 
